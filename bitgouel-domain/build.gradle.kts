b8aef045dd417e7eadade2582f58cffea2848120
aesperer <piyrw9754@gmail.com>
2023-10-05 23:08:32 +0900

    add :: jar, bootjar task false'

--- bitgouel-domain/build.gradle.kts
+++ bitgouel-domain/build.gradle.kts
@@ -1,6 +1,15 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+jar.enabled = true
+bootJar.enabled = false
+
 plugins {
 
 }
+
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
