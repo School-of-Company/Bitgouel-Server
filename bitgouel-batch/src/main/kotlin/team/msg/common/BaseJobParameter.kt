7f3f66d43320ec79139a4c5abb8d4d36a8c6b393
김희망 <105429536+esperar@users.noreply.github.com>
2023-10-07 19:47:56 +0900

    Update bitgouel-batch/src/main/kotlin/team/msg/common/BaseJobParameter.kt
    Co-authored-by: 노현주 <106813267+ani2689@users.noreply.github.com>

--- bitgouel-batch/src/main/kotlin/team/msg/common/BaseJobParameter.kt
+++ bitgouel-batch/src/main/kotlin/team/msg/common/BaseJobParameter.kt
@@ -4,5 +4,4 @@ package team.msg.common
  * 개발/QA 도중에 같은 Job 파라미터 값으로 배치 실행이 가능하도록 version을 지정합니다.
  * version은 1부터 시작합니다.
  */
-open class BaseJobParameter(val version: Int) {
-}
+open class BaseJobParameter(val version: Int)
