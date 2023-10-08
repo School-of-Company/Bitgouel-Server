0127a157725444a97bef23557079f08efbb0f880
JuuuuHong <juhang720@gmail.com>
2023-10-08 15:00:12 +0900

    add :: 여러 exception handler 추가

--- bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/error/ErrorCode.kt
+++ bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/error/ErrorCode.kt
@@ -4,5 +4,6 @@ enum class ErrorCode(
     val message: String,
     val status: Int
 ) {
-    INTERNAL_SERVER_ERROR("서버 내부 에러", 500)
+    INTERNAL_SERVER_ERROR("서버 내부 에러", 500),
+    BAD_REQUEST("잘못된 요청입니다.", 400)
 }
