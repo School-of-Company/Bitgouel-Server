50b6c63518b60e260202cc9ce0bc50ee7e2171d3
JuuuuHong <juhang720@gmail.com>
2023-10-08 15:04:33 +0900

    delete :: response entity

--- bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/error/handler/GlobalExceptionHandler.kt
+++ bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/error/handler/GlobalExceptionHandler.kt
@@ -6,8 +6,6 @@ import bitgouel.team.msg.global.error.NoHandlerErrorResponse
 import bitgouel.team.msg.global.error.ValidationErrorResponse
 import bitgouel.team.msg.global.error.exception.BitgouelException
 import org.springframework.dao.DataIntegrityViolationException
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -18,23 +16,18 @@ import org.springframework.web.servlet.NoHandlerFoundException
 class GlobalExceptionHandler {
 
     @ExceptionHandler(BitgouelException::class)
-    fun bitgouelExceptionHandler(e: BitgouelException): ResponseEntity<ErrorResponse> =
-        ResponseEntity(ErrorResponse.of(e), HttpStatus.valueOf(e.errorCode.status))
+    fun bitgouelExceptionHandler(e: BitgouelException): ErrorResponse = ErrorResponse.of(e)
 
     @ExceptionHandler(BindException::class)
-    fun handleBindException(e: BindException): ResponseEntity<ValidationErrorResponse> =
-        ResponseEntity(ErrorResponse.of(e), HttpStatus.BAD_REQUEST)
+    fun handleBindException(e: BindException): ValidationErrorResponse = ErrorResponse.of(e)
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ValidationErrorResponse> =
-        ResponseEntity(ErrorResponse.of(e), HttpStatus.BAD_REQUEST)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ValidationErrorResponse = ErrorResponse.of(e)
 
     @ExceptionHandler(DataIntegrityViolationException::class)
-    fun handleDataIntegrityViolationException(e: DataIntegrityViolationException): ResponseEntity<DataErrorResponse> =
-        ResponseEntity(ErrorResponse.of(e), HttpStatus.BAD_REQUEST)
+    fun handleDataIntegrityViolationException(e: DataIntegrityViolationException): DataErrorResponse = ErrorResponse.of(e)
 
     @ExceptionHandler(NoHandlerFoundException::class)
-    fun handleNoHandlerFoundException(e: NoHandlerFoundException): ResponseEntity<NoHandlerErrorResponse> =
-        ResponseEntity(ErrorResponse.of(e), HttpStatus.BAD_REQUEST)
+    fun handleNoHandlerFoundException(e: NoHandlerFoundException): NoHandlerErrorResponse = ErrorResponse.of(e)
 
 }
