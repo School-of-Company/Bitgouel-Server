2b9ea45ed755d4b6dc04d258d35d31cffe0fbaf9
JuuuuHong <juhang720@gmail.com>
2023-10-10 23:04:17 +0900

    add :: spring security dependency

--- bitgouel-api/build.gradle.kts
+++ bitgouel-api/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
 
     implementation(project(":bitgouel-domain"))
 }
