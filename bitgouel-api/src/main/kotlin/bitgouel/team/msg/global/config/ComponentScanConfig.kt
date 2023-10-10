733effd14e7f08a3a47067293360b43b7e32d4eb
JuuuuHong <juhang720@gmail.com>
2023-10-10 22:54:56 +0900

    add :: component scan

---
+++ bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/config/ComponentScanConfig.kt
@@ -0,0 +1,12 @@
+package bitgouel.team.msg.global.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan(
+    basePackages = [
+        "domain"
+    ]
+)
+class ComponentScanConfig
