92733fc1399e35e16381250c9bd266ec48fb498e
JuuuuHong <juhang720@gmail.com>
2023-10-16 08:32:51 +0900

    add :: custom authenticationEntryPoint handler

--- bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/security/handler/CustomAccessDeniedHandler.kt
+++ bitgouel-api/src/main/kotlin/bitgouel/team/msg/global/security/handler/CustomAccessDeniedHandler.kt
@@ -12,7 +12,7 @@ class CustomAccessDeniedHandler : AccessDeniedHandler {
     private val log = LoggerFactory.getLogger(this.javaClass.name)
 
     override fun handle(request: HttpServletRequest?,response: HttpServletResponse?,accessDeniedException: AccessDeniedException?) {
-        log.warn("Access Denied")
+        log.warn("========== Access Denied ==========")
         throw InvalidRoleException("검증되지 않은 권한입니다.")
     }
 }
