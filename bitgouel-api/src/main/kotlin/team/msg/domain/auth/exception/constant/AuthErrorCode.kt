cac7edb1134e2e73700ffa33763d49705ace40b0
JuuuuHong <juhang720@gmail.com>
2023-10-18 10:18:39 +0900

    update :: already exist exception error code 409

--- bitgouel-api/src/main/kotlin/team/msg/domain/auth/exception/constant/AuthErrorCode.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/auth/exception/constant/AuthErrorCode.kt
@@ -4,6 +4,6 @@ enum class AuthErrorCode(
     val message: String,
     val status: Int
 ) {
-    ALREADY_EXIST_EMAIL("이미 가입된 이메일입니다.", 403),
-    ALREADY_EXIST_PHONE_NUMBER("이미 가입된 전화번호입니다..", 403)
+    ALREADY_EXIST_EMAIL("이미 가입된 이메일입니다.", 409),
+    ALREADY_EXIST_PHONE_NUMBER("이미 가입된 전화번호입니다..", 409)
 }
