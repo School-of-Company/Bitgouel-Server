e908ee776bc0084f39e4407adfec4c78f962216e
JuuuuHong <juhang720@gmail.com>
2023-10-18 17:54:11 +0900

    add :: teacher sign up api

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/auth/presentation/data/web/TeacherSignUpWebRequest.kt
@@ -0,0 +1,12 @@
+package team.msg.domain.auth.presentation.data.web
+
+import team.msg.domain.school.enums.HighSchool
+
+class TeacherSignUpWebRequest(
+    val email: String,
+    val name: String,
+    val phoneNumber: String,
+    val password: String,
+    val highSchool: HighSchool,
+    val clubName: String
+)
