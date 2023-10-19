c4ebaa56da211738a3eafdef0c6c4a61664d4e08
ani2689 <ojno7777@gmail.com>
2023-10-19 16:42:22 +0900

    add :: 주석

--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapperImpl.kt
@@ -6,6 +6,10 @@ import team.msg.domain.lecture.presentation.data.web.LectureCreateWebRequest
 
 @Component
 class LectureRequestMapperImpl : LectureRequestMapper{
+    /**
+     * Lecture 개설 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     * @param LectureCreateWebRequest
+     */
     override fun lectureCreateWebRequestToDto(request: LectureCreateWebRequest) = LectureCreateRequest(
         request.name,
         request.content,
