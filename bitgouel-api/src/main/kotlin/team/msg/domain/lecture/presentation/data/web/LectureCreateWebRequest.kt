236319dea9d77d769af83fce95a6fb7396a7d476
ani2689 <ojno7777@gmail.com>
2023-10-19 09:39:38 +0900

    create :: web request validation

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/presentation/data/web/LectureCreateWebRequest.kt
@@ -0,0 +1,39 @@
+package team.msg.domain.lecture.presentation.data.web
+
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.validation.constraints.Future
+import javax.validation.constraints.FutureOrPresent
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import team.msg.domain.lecture.enum.LectureType
+import java.time.LocalDateTime
+
+data class LectureCreateWebRequest(
+    @field:NotBlank
+    val name: String,
+
+    @field:NotBlank
+    val content: String,
+
+    @field:NotNull
+    @FutureOrPresent
+    val startDate: LocalDateTime,
+
+    @field:NotNull
+    @Future
+    val endDate: LocalDateTime,
+
+    @field:NotNull
+    @Future
+    val completeDate: LocalDateTime,
+
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    val lectureType: LectureType,
+
+    @field:NotNull
+    @field:Min(1)
+    val maxRegisteredUser: Int
+)
