63b9cc4b985364135e625baabaea07cbaed91cde
KimTaeO <taeo0328@gmail.com>
2023-10-19 19:39:29 +0900

    add :: create student activity service

--- bitgouel-domain/src/main/kotlin/team/msg/domain/teacher/repository/TeacherRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/teacher/repository/TeacherRepository.kt
@@ -1,8 +1,10 @@
 package team.msg.domain.teacher.repository
 
 import org.springframework.data.repository.CrudRepository
+import team.msg.domain.club.model.Club
 import team.msg.domain.teacher.model.Teacher
 import java.util.UUID
 
 interface TeacherRepository : CrudRepository<Teacher, UUID> {
+    fun findByClub(club: Club): Teacher?
 }
