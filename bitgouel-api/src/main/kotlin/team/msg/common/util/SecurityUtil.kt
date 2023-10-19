21d58f5f0a8fdc9198b91ca529f6d26fd0b10a5d
ani2689 <ojno7777@gmail.com>
2023-10-19 20:02:01 +0900

    add :: query current user

--- bitgouel-api/src/main/kotlin/team/msg/common/util/SecurityUtil.kt
+++ bitgouel-api/src/main/kotlin/team/msg/common/util/SecurityUtil.kt
@@ -1,12 +1,31 @@
 package team.msg.common.util
 
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
+import team.msg.domain.user.exception.UserNotFoundException
+import team.msg.domain.user.model.User
+import team.msg.domain.user.repository.UserRepository
+import java.util.*
 
 @Component
 class SecurityUtil(
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val userRepository: UserRepository
 ) {
     fun passwordEncode(password: String): String =
         passwordEncoder.encode(password)
+
+    /**
+     * 현재 로그인 된 유저를 찾는 로직
+     */
+    fun queryCurrentUser(): User {
+        val currentUserId = queryCurrentUserId()
+        return userRepository.findByIdOrNull(currentUserId)
+            ?: throw UserNotFoundException("로그인 된 유저를 찾을 수 없습니다. values : [ UUID = $currentUserId ]")
+    }
+    private fun queryCurrentUserId() =
+        UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+
 }
