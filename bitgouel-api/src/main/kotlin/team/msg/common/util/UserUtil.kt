f942e4bf659c5c6b43cf769ed81d04787f16386b
KimTaeO <taeo0328@gmail.com>
2023-10-19 18:26:37 +0900

    add :: token의 UUID로 User 반환 공통로직

---
+++ bitgouel-api/src/main/kotlin/team/msg/common/util/UserUtil.kt
@@ -0,0 +1,26 @@
+package team.msg.common.util
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import team.msg.domain.user.exception.UserNotFoundException
+import team.msg.domain.user.model.User
+import team.msg.domain.user.repository.UserRepository
+import team.msg.global.security.principal.AuthDetails
+import java.util.*
+
+@Component
+class UserUtil(
+    private val userRepository: UserRepository
+) {
+    fun queryCurrentUser(): User {
+        val principal = SecurityContextHolder.getContext().authentication.principal
+
+        val userId = UUID.fromString(if(principal is AuthDetails)
+            principal.username
+        else
+            principal.toString())
+
+        return userRepository.findByIdOrNull(userId) ?: throw UserNotFoundException("존재하지 않는 유저입니다.")
+    }
+}
