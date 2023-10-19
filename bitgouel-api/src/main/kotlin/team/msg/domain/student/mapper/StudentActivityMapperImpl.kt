038243acebc6d9c77e89a9728a7445e5113ea8d6
KimTaeO <taeo0328@gmail.com>
2023-10-19 19:46:12 +0900

    add :: student activity mapper에 Component annotation

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/mapper/StudentActivityMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/mapper/StudentActivityMapperImpl.kt
@@ -1,8 +1,10 @@
 package team.msg.domain.student.mapper
 
+import org.springframework.stereotype.Component
 import team.msg.domain.student.presentation.data.request.CreateStudentActivityRequest
 import team.msg.domain.student.presentation.data.web.CreateStudentActivityWebRequest
 
+@Component
 class StudentActivityMapperImpl : StudentActivityMapper {
     override fun studentActivityWebRequestToDto(webRequest: CreateStudentActivityWebRequest): CreateStudentActivityRequest =
         CreateStudentActivityRequest(
