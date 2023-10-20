81dd9d275ede7802499220b296869c2965f03621
KimTaeO <taeo0328@gmail.com>
2023-10-20 11:15:39 +0900

    chore :: create student activity 주석

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/mapper/StudentActivityMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/mapper/StudentActivityMapperImpl.kt
@@ -6,6 +6,10 @@ import team.msg.domain.student.presentation.data.web.CreateStudentActivityWebReq
 
 @Component
 class StudentActivityMapperImpl : StudentActivityMapper {
+
+    /**
+     * StudentActivity 생성 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
     override fun createStudentActivityWebRequestToDto(webRequest: CreateStudentActivityWebRequest): CreateStudentActivityRequest =
         CreateStudentActivityRequest(
             title = webRequest.title,
