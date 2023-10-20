ad168d158c4975e49ae3c048fa604a63d4ae7c70
KimTaeO <taeo0328@gmail.com>
2023-10-20 23:12:59 +0900

    add :: update student activity service

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/service/StudentActivityServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/service/StudentActivityServiceImpl.kt
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.msg.common.enum.ApproveStatus
 import team.msg.common.util.UserUtil
+import team.msg.domain.student.exception.StudentActivityNotFoundException
 import team.msg.domain.student.exception.StudentNotFoundException
 import team.msg.domain.student.model.StudentActivity
 import team.msg.domain.student.presentation.data.request.CreateStudentActivityRequest
+import team.msg.domain.student.presentation.data.request.UpdateStudentActivityRequest
 import team.msg.domain.student.repository.StudentActivityRepository
 import team.msg.domain.student.repository.StudentRepository
 import team.msg.domain.teacher.exception.TeacherNotFoundException
@@ -47,5 +49,35 @@ class StudentActivityServiceImpl(
         )
 
         studentActivityRepository.save(studentActivity)
+    }
+
+
+    /**
+     * 학생 활동을 업데이트하는 비지니스 로직입니다
+     * @param UpdateStudentActivityRequest
+     */
+    @Transactional(rollbackFor = [Exception::class])
+    override fun updateStudentActivity(id: UUID,request: UpdateStudentActivityRequest) {
+        val user = userUtil.queryCurrentUser()
+
+        val student = studentRepository.findByUser(user)
+            ?: throw StudentNotFoundException("학생을 찾을 수 없습니다. info : [ name = ${user.name} ]")
+
+        val studentActivity = studentActivityRepository.findByIdAndStudent(id, student)
+            ?: throw StudentActivityNotFoundException("학생 활동을 찾을 수 없습니다. info : [ id = $id ]")
+
+        val updatedStudentActivity = StudentActivity(
+            id = studentActivity.id,
+            title = request.title,
+            content = request.content,
+            credit = request.credit,
+            activityDate = request.activityDate,
+            createdAt = studentActivity.createdAt,
+            approveStatus = studentActivity.approveStatus,
+            student = studentActivity.student,
+            teacher = studentActivity.teacher,
+        )
+
+        studentActivityRepository.save(updatedStudentActivity)
     }
 }
--- bitgouel-domain/src/main/kotlin/team/msg/domain/student/repository/StudentActivityRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/student/repository/StudentActivityRepository.kt
@@ -1,7 +1,10 @@
 package team.msg.domain.student.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import team.msg.domain.student.model.Student
 import team.msg.domain.student.model.StudentActivity
-import java.util.UUID
+import java.util.*
 
-interface StudentActivityRepository : JpaRepository<StudentActivity, UUID>
+interface StudentActivityRepository : JpaRepository<StudentActivity, UUID> {
+    fun findByIdAndStudent(id: UUID, student: Student): StudentActivity?
+}
