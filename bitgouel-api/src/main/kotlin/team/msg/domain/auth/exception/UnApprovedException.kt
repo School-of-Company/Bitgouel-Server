a4a94d72ab3da89b03b5bc25f49713776a0a27b1
JuuuuHong <juhang720@gmail.com>
2023-10-20 10:43:17 +0900

    update :: un approved exception

--- bitgouel-api/src/main/kotlin/team/msg/domain/auth/exception/UnApprovedException.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/auth/exception/UnApprovedException.kt
@@ -3,6 +3,6 @@ package team.msg.domain.auth.exception
 import team.msg.domain.auth.exception.constant.AuthErrorCode
 import team.msg.global.error.exception.BitgouelException
 
-class NotApprovedException(
+class UnApprovedException(
     message: String
 ) : BitgouelException(message, AuthErrorCode.NOT_APPROVED.status)
