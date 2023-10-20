d60a251c7e7c07e11ec755158d8eeca3fa943178
KimTaeO <taeo0328@gmail.com>
2023-10-20 23:13:25 +0900

    add :: update student activity controller

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/presentation/StudentActivityController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/presentation/StudentActivityController.kt
@@ -3,13 +3,17 @@ package team.msg.domain.student.presentation
 import javax.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.msg.domain.student.mapper.StudentActivityMapper
 import team.msg.domain.student.presentation.data.web.CreateStudentActivityWebRequest
+import team.msg.domain.student.presentation.data.web.UpdateStudentActivityWebRequest
 import team.msg.domain.student.service.StudentActivityService
+import java.util.UUID
 
 @RestController
 @RequestMapping("/activity")
@@ -21,5 +25,11 @@ class StudentActivityController(
     fun createStudentActivity(@RequestBody @Valid request: CreateStudentActivityWebRequest): ResponseEntity<Void> {
         studentActivityService.createStudentActivity(studentActivityMapper.createStudentActivityWebRequestToDto(request))
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @PatchMapping("/{id}")
+    fun updateStudentActivity(@PathVariable id: UUID, @RequestBody @Valid webRequest: UpdateStudentActivityWebRequest): ResponseEntity<Void> {
+        studentActivityService.updateStudentActivity(id, studentActivityMapper.updateStudentActivityWebRequestToDto(webRequest))
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
