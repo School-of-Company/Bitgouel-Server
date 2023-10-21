3c5b22efad7441406dfa9dd399882cc5e7153998
KimTaeO <taeo0328@gmail.com>
2023-10-21 23:35:11 +0900

    add :: LastModifiedAt property 추가

--- bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseTimeEntity.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseTimeEntity.kt
@@ -2,10 +2,16 @@ package team.msg.common.entity
 
 import javax.persistence.Column
 import javax.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
 
 @MappedSuperclass
 abstract class BaseTimeEntity(
     @Column(nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
-    open val createdAt: LocalDateTime = LocalDateTime.now()
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(nullable = false, columnDefinition = "DATETIME(6)")
+    var modifiedAt: LocalDateTime = LocalDateTime.now()
 )
