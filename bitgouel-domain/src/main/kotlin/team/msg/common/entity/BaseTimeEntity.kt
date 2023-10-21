4382c4848280969399c308098d4ad560a57923ea
KimTaeO <taeo0328@gmail.com>
2023-10-21 23:36:40 +0900

    update :: property들 field로 이동 및 setter 접근 제한

--- bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseTimeEntity.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseTimeEntity.kt
@@ -7,12 +7,14 @@ import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
 
 @MappedSuperclass
-abstract class BaseTimeEntity(
+abstract class BaseTimeEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
-    var createdAt: LocalDateTime = LocalDateTime.now(),
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
 
     @LastModifiedDate
     @Column(nullable = false, columnDefinition = "DATETIME(6)")
     var modifiedAt: LocalDateTime = LocalDateTime.now()
-)
+        protected set
+}
