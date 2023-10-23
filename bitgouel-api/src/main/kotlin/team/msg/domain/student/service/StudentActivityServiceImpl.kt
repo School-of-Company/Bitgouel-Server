d91849a3533f3676adc5f4b4d24cf87789c8a318
aesperer <piyrw9754@gmail.com>
2023-10-23 15:06:49 +0900

    chore :: 주석 @param 더 자세히 설명

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/service/StudentActivityServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/service/StudentActivityServiceImpl.kt
@@ -57,7 +57,7 @@ class StudentActivityServiceImpl(
     /**
      * 학생 활동을 업데이트하는 비지니스 로직입니다
      * applicationEventPublisher로부터 학생 활동 업데이트 이벤트를 발행합니다.
-     * @param 학생 활동을 수정하기 위해 데이터를 담은 request Dto와 학생 정보 활동 id
+     * @param 학생 활동 id, 학생 활동을 수정하기 위한 데이터들을 담은 request Dto,
      */
     @Transactional(rollbackFor = [Exception::class])
     override fun updateStudentActivity(id: UUID, request: UpdateStudentActivityRequest) {
