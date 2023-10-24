4186e6d9f35cd73f2fa68e642d1a4c7eea909cbd
JuuuuHong <juhang720@gmail.com>
2023-10-24 20:51:04 +0900

    add :: 지연로딩

--- bitgouel-domain/src/main/kotlin/team/msg/domain/fAQ/model/FAQ.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/fAQ/model/FAQ.kt
@@ -2,6 +2,7 @@ package team.msg.domain.fAQ.model
 
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
@@ -22,7 +23,7 @@ class FAQ(
     @Column(nullable = false)
     val answer: String,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admin_id", columnDefinition = "BINARY(16)")
     val admin: Admin
 
