7603fd31d06f905bf08e1d9b5287a2ff31091f06
JuuuuHong <juhang720@gmail.com>
2023-10-24 20:09:54 +0900

    create :: faq repository

---
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/fAQ/repository/FAQRepository.kt
@@ -0,0 +1,8 @@
+package team.msg.domain.fAQ.repository
+
+import org.springframework.data.repository.CrudRepository
+import team.msg.domain.fAQ.model.FAQ
+import java.util.*
+
+interface FAQRepository : CrudRepository<FAQ,UUID> {
+}
