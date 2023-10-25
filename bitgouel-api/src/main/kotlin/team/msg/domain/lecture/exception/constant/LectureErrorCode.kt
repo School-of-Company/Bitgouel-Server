f3ee57c423f4e5f9142787793c48449554d3ad78
ani2689 <ojno7777@gmail.com>
2023-10-25 20:02:41 +0900

    delete :: exception message

--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/exception/constant/LectureErrorCode.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/exception/constant/LectureErrorCode.kt
@@ -1,12 +1,13 @@
 package team.msg.domain.lecture.exception.constant
 
 enum class LectureErrorCode(
-    val message: String,
     val status: Int
 ){
-    INVALID_LECTURE_TYPE("유효하지 않은 강의 구분입니다.", 400),
+    INVALID_LECTURE_TYPE(400),
 
-    LECTURE_NOT_FOUND("존재하지 않는 강의입니다.", 404),
+    LECTURE_NOT_FOUND(404),
 
-    ALREADY_APPROVED_LECTURE("이미 개설 신청이 승인된 강의입니다.",409)
+    ALREADY_APPROVED_LECTURE(409),
+    NOT_APPROVED_LECTURE(409),
+    OVER_MAX_REGISTERED_USER(409)
 }
