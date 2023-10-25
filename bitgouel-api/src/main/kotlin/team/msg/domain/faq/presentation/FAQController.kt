30b3e9be2493f2dae8e4fdc8352a16b2346d7fbc
JuuuuHong <juhang720@gmail.com>
2023-10-25 23:10:13 +0900

    add :: faq api

--- bitgouel-api/src/main/kotlin/team/msg/domain/faq/presentation/FAQController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/faq/presentation/FAQController.kt
@@ -4,12 +4,14 @@ import javax.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.msg.domain.faq.mapper.FaqRequestMapper
 import team.msg.domain.faq.presentation.data.response.QueryAllFaqsResponse
+import team.msg.domain.faq.presentation.data.response.QueryFaqDetailsResponse
 import team.msg.domain.faq.presentation.web.CreateFaqWebRequest
 import team.msg.domain.faq.service.FaqService
 
@@ -29,6 +31,12 @@ class FaqController(
     @GetMapping
     fun queryAllFaqs(): ResponseEntity<List<QueryAllFaqsResponse>> {
         val response = faqService.queryAllFaqs()
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/{id}")
+    fun queryFaqDetails(@PathVariable id: Long): ResponseEntity<QueryFaqDetailsResponse> {
+        val response = faqService.queryFaqDetails(id)
         return ResponseEntity.ok(response)
     }
 }
