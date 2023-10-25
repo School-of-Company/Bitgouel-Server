b3f3e8b8138affcb6d071bee256444910f26c9fd
JuuuuHong <juhang720@gmail.com>
2023-10-25 20:30:11 +0900

    add :: / url

--- bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
@@ -69,7 +69,7 @@ class SecurityConfig(
 
             // faq
             .mvcMatchers(HttpMethod.POST, "/FAQ").hasRole(ADMIN)
-            .mvcMatchers(HttpMethod.GET, "FAQ").authenticated()
+            .mvcMatchers(HttpMethod.GET, "/FAQ").authenticated()
 
             .anyRequest().authenticated()
             .and()
