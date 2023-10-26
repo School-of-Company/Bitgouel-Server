8f2228ce366e0ab4502a66337f27767f0d640b2f
KimTaeO <taeo0328@gmail.com>
2023-10-26 14:50:50 +0900

    create :: student activity response

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/presentation/data/response/StudentActivityResponse.kt
@@ -0,0 +1,30 @@
+package team.msg.domain.student.presentation.data.response
+
+import org.springframework.data.domain.Page
+import team.msg.common.enum.ApproveStatus
+import team.msg.domain.student.model.StudentActivity
+import team.msg.domain.user.model.User
+import java.util.*
+
+data class StudentActivityResponse(
+    val activityId: UUID,
+    val title: String,
+    val userId: UUID,
+    val username: String,
+    val approveStatus: ApproveStatus
+) {
+    companion object {
+        fun of(studentActivity: StudentActivity, user: User) =
+            StudentActivityResponse(
+                activityId = studentActivity.id,
+                title = studentActivity.title,
+                userId = user.id,
+                username = user.name,
+                approveStatus = user.approveStatus
+            )
+    }
+}
+
+data class AllStudentActivityResponse(
+    val activities: Page<StudentActivityResponse>
+)
