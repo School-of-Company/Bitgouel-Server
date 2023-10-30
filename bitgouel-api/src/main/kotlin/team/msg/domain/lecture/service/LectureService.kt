3cd683f3bf96ea4c63d744c0106651544b090b06
ani2689 <ojno7777@gmail.com>
2023-10-30 15:07:48 +0900

    add :: 강의 조회 기능 추상화

--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/service/LectureService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/service/LectureService.kt
@@ -1,10 +1,15 @@
 package team.msg.domain.lecture.service
 
+import team.msg.domain.lecture.enum.LectureStatus
 import team.msg.domain.lecture.presentation.data.request.CreateLectureRequest
+import team.msg.domain.lecture.presentation.data.response.AllLecturesResponse
+import team.msg.domain.lecture.presentation.data.response.LectureDetailsResponse
 import java.util.UUID
 
 interface LectureService {
     fun createLecture(request: CreateLectureRequest)
+    fun queryAllLectures(status: LectureStatus): AllLecturesResponse
+    fun queryLectureDetails(id: UUID): LectureDetailsResponse
     fun signUpLecture(id: UUID)
     fun approveLecture(id: UUID)
     fun rejectLecture(id: UUID)
