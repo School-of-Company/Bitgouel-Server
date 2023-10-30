c9d8dccef0e8518674d93ca33ceeb6737e361338
ani2689 <ojno7777@gmail.com>
2023-10-31 00:11:05 +0900

    add :: 강의 리스트 조회 mapper

--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapper.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapper.kt
@@ -1,8 +1,11 @@
 package team.msg.domain.lecture.mapper
 
 import team.msg.domain.lecture.presentation.data.request.CreateLectureRequest
+import team.msg.domain.lecture.presentation.data.request.QueryAllLectureRequest
 import team.msg.domain.lecture.presentation.web.CreateLectureWebRequest
+import team.msg.domain.lecture.presentation.web.QueryAllLecturesWebRequest
 
 interface LectureRequestMapper {
-    fun createLectureWebRequestToDto(webRequest: CreateLectureWebRequest) : CreateLectureRequest
+    fun createLectureWebRequestToDto(webRequest: CreateLectureWebRequest): CreateLectureRequest
+    fun queryLectureWebRequestToDto(webRequest: QueryAllLecturesWebRequest): QueryAllLectureRequest
 }
--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/mapper/LectureRequestMapperImpl.kt
@@ -2,7 +2,9 @@ package team.msg.domain.lecture.mapper
 
 import org.springframework.stereotype.Component
 import team.msg.domain.lecture.presentation.data.request.CreateLectureRequest
+import team.msg.domain.lecture.presentation.data.request.QueryAllLectureRequest
 import team.msg.domain.lecture.presentation.web.CreateLectureWebRequest
+import team.msg.domain.lecture.presentation.web.QueryAllLecturesWebRequest
 
 @Component
 class LectureRequestMapperImpl : LectureRequestMapper{
@@ -18,5 +20,14 @@ class LectureRequestMapperImpl : LectureRequestMapper{
         lectureType = webRequest.lectureType,
         credit = webRequest.credit,
         maxRegisteredUser = webRequest.maxRegisteredUser
+    )
+
+    /**
+     * Lecture list 검색 web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
+    override fun queryLectureWebRequestToDto(webRequest: QueryAllLecturesWebRequest) = QueryAllLectureRequest(
+        lectureType = webRequest.lectureType,
+        lectureStatus = webRequest.lectureStatus,
+        approveStatus = webRequest.approveStatus
     )
 }
