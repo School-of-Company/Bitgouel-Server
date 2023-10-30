046b29375fe282a72ee4d807f784832983738cec
ani2689 <ojno7777@gmail.com>
2023-10-30 14:59:12 +0900

    create :: 강의 response

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/presentation/data/response/LectureResponse.kt
@@ -0,0 +1,67 @@
+package team.msg.domain.lecture.presentation.data.response
+import org.springframework.data.domain.Page
+import team.msg.domain.lecture.enum.LectureStatus
+import team.msg.domain.lecture.enum.LectureType
+import team.msg.domain.lecture.model.Lecture
+import java.time.LocalDateTime
+import java.util.*
+
+data class LectureResponse(
+    val id: UUID,
+    val name: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val completeDate: LocalDateTime,
+    val lectureType: LectureType,
+    val lectureStatus: LectureStatus,
+    val headCount: Int,
+    val maxRegisteredUser: Int
+) {
+    companion object {
+        fun of(lecture: Lecture, headCount: Int): LectureResponse = LectureResponse(
+            id = lecture.id,
+            name = lecture.name,
+            startDate = lecture.startDate,
+            endDate = lecture.endDate,
+            completeDate = lecture.completeDate,
+            lectureType = lecture.lectureType,
+            lectureStatus = lecture.getLectureStatus(),
+            headCount = headCount,
+            maxRegisteredUser = lecture.maxRegisteredUser
+        )
+
+        fun detailOf(lecture: Lecture, headCount: Int, lecturer: String): LectureDetailsResponse = LectureDetailsResponse(
+            name = lecture.name,
+            content = lecture.content,
+            createAt = lecture.createdAt,
+            startDate = lecture.startDate,
+            endDate = lecture.endDate,
+            completedDate = lecture.completeDate,
+            lectureType = lecture.lectureType,
+            lectureStatus = lecture.getLectureStatus(),
+            headCount = headCount,
+            maxRegisteredUser = lecture.maxRegisteredUser,
+            lecturer = lecturer,
+            credit = lecture.credit
+        )
+    }
+}
+
+data class AllLecturesResponse(
+    val lectures: Page<LectureResponse>
+)
+
+data class LectureDetailsResponse(
+    val name: String,
+    val content: String,
+    val createAt: LocalDateTime,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val completedDate: LocalDateTime,
+    val lectureType: LectureType,
+    val lectureStatus: LectureStatus,
+    val headCount: Int,
+    val maxRegisteredUser: Int,
+    val lecturer: String,
+    val credit: Int
+)
