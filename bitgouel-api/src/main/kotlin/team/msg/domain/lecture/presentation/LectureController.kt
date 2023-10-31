3ad806a4eba6ff71b5bc881d6a881fde1c62bd4c
ani2689 <ojno7777@gmail.com>
2023-10-31 13:38:41 +0900

    add :: query all lectures

--- bitgouel-api/src/main/kotlin/team/msg/domain/lecture/presentation/LectureController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/lecture/presentation/LectureController.kt
@@ -1,17 +1,22 @@
 package team.msg.domain.lecture.presentation
 
 import javax.validation.Valid
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.msg.domain.lecture.mapper.LectureRequestMapper
+import team.msg.domain.lecture.presentation.data.response.AllLecturesResponse
 import team.msg.domain.lecture.presentation.web.CreateLectureWebRequest
+import team.msg.domain.lecture.presentation.web.QueryAllLecturesWebRequest
 import team.msg.domain.lecture.service.LectureService
 import java.util.*
 
@@ -26,6 +31,13 @@ class LectureController(
         val request = lectureRequestMapper.createLectureWebRequestToDto(webRequest)
         lectureService.createLecture(request)
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping
+    fun queryAllLectures(pageable: Pageable, @RequestParam(required = false) queryAllLecturesWebRequest: QueryAllLecturesWebRequest): ResponseEntity<AllLecturesResponse>{
+        val request = lectureRequestMapper.queryLectureWebRequestToDto(queryAllLecturesWebRequest)
+        val response = lectureService.queryAllLectures(pageable, request)
+        return ResponseEntity.status(HttpStatus.OK).body(response)
     }
 
     @PostMapping("/{id}")
