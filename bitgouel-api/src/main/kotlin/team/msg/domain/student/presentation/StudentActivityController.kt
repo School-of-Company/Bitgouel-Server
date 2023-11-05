7c967668143cab0b20a664ede71c9b32165d5d34
KimTaeO <taeo0328@gmail.com>
2023-11-06 08:44:50 +0900

    add :: My Student Activities controller

--- bitgouel-api/src/main/kotlin/team/msg/domain/student/presentation/StudentActivityController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/student/presentation/StudentActivityController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.msg.domain.student.mapper.StudentActivityMapper
 import team.msg.domain.student.presentation.data.response.AllStudentActivitiesResponse
+import team.msg.domain.student.presentation.data.response.MyStudentActivitiesByStudentResponse
 import team.msg.domain.student.presentation.data.web.CreateStudentActivityWebRequest
 import team.msg.domain.student.presentation.data.web.UpdateStudentActivityWebRequest
 import team.msg.domain.student.service.StudentActivityService
@@ -65,7 +66,13 @@ class StudentActivityController(
 
     @GetMapping("/{student_id}")
     fun queryStudentActivityByStudent(@PathVariable("student_id") studentId: UUID, pageable: Pageable): ResponseEntity<AllStudentActivitiesResponse> {
-        val response = studentActivityService.queryStudentActivityByStudent(studentId, pageable)
+        val response = studentActivityService.queryStudentActivitiesByStudent(studentId, pageable)
+        return ResponseEntity.status(HttpStatus.OK).body(response)
+    }
+
+    @GetMapping("/my")
+    fun queryMyStudentActivities(pageable: Pageable): ResponseEntity<MyStudentActivitiesByStudentResponse> {
+        val response = studentActivityService.queryMyStudentActivity(pageable)
         return ResponseEntity.status(HttpStatus.OK).body(response)
     }
 }
