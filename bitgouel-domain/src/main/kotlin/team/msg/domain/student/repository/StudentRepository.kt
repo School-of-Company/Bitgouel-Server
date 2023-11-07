d122bf782dd6a3d9797448c1e8f03f00138eec5e
aesperer <piyrw9754@gmail.com>
2023-11-07 09:31:25 +0900

    update :: student activity는 teacher를 로딩하지 않으므로 fetch join으로 가져오지 않습니다 향후 변경 가능성 있음

--- bitgouel-domain/src/main/kotlin/team/msg/domain/student/repository/StudentRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/student/repository/StudentRepository.kt
@@ -1,5 +1,6 @@
 package team.msg.domain.student.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import team.msg.domain.student.model.Student
@@ -7,7 +8,9 @@ import team.msg.domain.user.model.User
 import java.util.*
 
 interface StudentRepository : CrudRepository<Student, UUID> {
+
+    @EntityGraph(attributePaths = ["user"], type = EntityGraph.EntityGraphType.FETCH)
     fun findByUser(user: User): Student?
-    @Query("SELECT student FROM Student student JOIN FETCH student.user WHERE student.id = :id")
+    @EntityGraph(attributePaths = ["user"], type = EntityGraph.EntityGraphType.FETCH)
     fun findStudentById(id: UUID): Student?
 }
