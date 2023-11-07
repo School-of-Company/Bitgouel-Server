acf44fd7bff339a28f3b0d1d058d68a31e860975
ani2689 <ojno7777@gmail.com>
2023-11-07 15:07:56 +0900

    update :: 함수 간소화

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/LectureRepositoryCustomImpl.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/LectureRepositoryCustomImpl.kt
@@ -2,13 +2,11 @@ package team.msg.domain.lecture.repository.custom
 
 import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 import team.msg.common.enums.ApproveStatus
 import team.msg.domain.lecture.enums.LectureType
-import team.msg.domain.lecture.model.Lecture
 import team.msg.domain.lecture.model.QLecture.lecture
 import team.msg.domain.user.model.QUser.user
 
@@ -16,8 +14,8 @@ import team.msg.domain.user.model.QUser.user
 class LectureRepositoryCustomImpl(
     private val queryFactory: JPAQueryFactory
 ) : LectureRepositoryCustom {
-    override fun findAllByApproveStatusAndLectureType(pageable: Pageable, approveStatus: ApproveStatus?, lectureType: LectureType?): Page<Lecture> {
-        val response = queryFactory
+    override fun findAllByApproveStatusAndLectureType(pageable: Pageable, approveStatus: ApproveStatus?, lectureType: LectureType?) = PageImpl(
+        queryFactory
             .selectFrom(lecture)
             .leftJoin(lecture.user, user)
             .fetchJoin()
@@ -27,10 +25,7 @@ class LectureRepositoryCustomImpl(
             )
             .offset(pageable.offset)
             .limit(pageable.pageSize.toLong())
-            .fetch()
-
-        return PageImpl(response)
-    }
+            .fetch())
 
     private fun eqLectureType(lectureType: LectureType?): BooleanExpression? {
         lectureType ?: return null
