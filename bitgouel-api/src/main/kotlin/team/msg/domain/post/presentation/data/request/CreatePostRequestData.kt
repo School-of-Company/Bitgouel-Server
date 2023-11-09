6fa7b7709123372fb0426a6f0536635ed6808b55
ani2689 <ojno7777@gmail.com>
2023-11-09 20:45:27 +0900

    update :: feed type

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/data/request/CreatePostRequestData.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/data/request/CreatePostRequestData.kt
@@ -1,7 +1,10 @@
 package team.msg.domain.post.presentation.data.request
 
+import team.msg.domain.post.enums.FeedType
+
 data class CreatePostRequestData(
     val title: String,
     val content: String,
-    val link: String?
+    val link: String?,
+    val feedType: FeedType
 )
