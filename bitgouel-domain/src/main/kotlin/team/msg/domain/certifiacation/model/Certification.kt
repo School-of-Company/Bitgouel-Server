7a9173c281046755c9ee209a4d147eb4112b5bc9
JuuuuHong <juhang720@gmail.com>
2023-11-13 17:48:37 +0900

    update :: 자격증 엔티티 id 참조로 변경

--- bitgouel-domain/src/main/kotlin/team/msg/domain/certifiacation/model/Certification.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/certifiacation/model/Certification.kt
@@ -2,11 +2,7 @@ package team.msg.domain.certifiacation.model
 
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
 import team.msg.common.entity.BaseUUIDEntity
-import team.msg.domain.user.model.User
 import java.time.LocalDate
 import java.util.*
 
@@ -16,14 +12,13 @@ class Certification(
     @get:JvmName("getIdentifier")
     override var id: UUID,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
-    val user: User,
+    @Column(name = "student_id", columnDefinition = "BINARY(16)")
+    val studentId: UUID,
 
     @Column(columnDefinition = "VARCHAR(30)", nullable = false)
     val name: String,
 
     @Column(columnDefinition = "DATE", nullable = false)
-    val acquisitionDate: LocalDate,
+    val acquisitionDate: LocalDate
 
 ) : BaseUUIDEntity(id)
