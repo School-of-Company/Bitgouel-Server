e1862b2c86d8fa7cd6c8b9e307bbecb43ed0d104
ani2689 <ojno7777@gmail.com>
2023-11-14 14:58:56 +0900

    update :: feed type request body로 받기

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/PostController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/PostController.kt
@@ -4,9 +4,7 @@ import javax.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import team.msg.domain.post.enums.FeedType
 import team.msg.domain.post.mapper.PostRequestMapper
 import team.msg.domain.post.presentation.web.CreatePostWebRequest
 import team.msg.domain.post.service.PostService
@@ -18,8 +16,8 @@ class PostController(
     private val postRequestMapper: PostRequestMapper
 ) {
     @PostMapping
-    fun createPost(@RequestBody @Valid webRequest: CreatePostWebRequest, @RequestParam(name = "type") FeedType: FeedType){
-        val request = postRequestMapper.createPostWebRequestToDto(webRequest, FeedType)
+    fun createPost(@RequestBody @Valid webRequest: CreatePostWebRequest){
+        val request = postRequestMapper.createPostWebRequestToDto(webRequest)
         postService.createPostService(request)
     }
 }
