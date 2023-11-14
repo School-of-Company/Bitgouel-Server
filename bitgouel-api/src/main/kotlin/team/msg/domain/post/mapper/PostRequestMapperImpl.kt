ef1dafa77cec323852e464ad36e8a7a39daf7e05
ani2689 <ojno7777@gmail.com>
2023-11-14 13:47:20 +0900

    update :: mapper 주석

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapperImpl.kt
@@ -7,6 +7,9 @@ import team.msg.domain.post.presentation.web.CreatePostWebRequest
 
 @Component
 class PostRequestMapperImpl : PostRequestMapper {
+    /**
+     * post 생성 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
     override fun createPostWebRequestToDto(webRequest: CreatePostWebRequest, feedType: FeedType) = CreatePostRequest(
         title = webRequest.title,
         content = webRequest.content,
