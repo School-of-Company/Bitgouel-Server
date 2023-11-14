07e1e37142569b5cd6b874bc926553f6c7315445
ani2689 <ojno7777@gmail.com>
2023-11-14 13:41:24 +0900

    update :: url size 제한

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/web/CreatePostWebRequest.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/presentation/web/CreatePostWebRequest.kt
@@ -4,6 +4,7 @@ import javax.validation.Valid
 import javax.validation.constraints.Max
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 import org.hibernate.validator.constraints.URL
 
 data class CreatePostWebRequest(
@@ -22,6 +23,7 @@ data class CreatePostWebRequest(
     data class Link(
         @field:URL
         @field:NotBlank
+        @field:Size(max=2083)
         val url: String
     )
 }
