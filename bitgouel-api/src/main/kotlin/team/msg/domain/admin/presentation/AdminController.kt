ab642c902f252766edb53a7c6b3f62ed51842974
KimTaeO <taeo0328@gmail.com>
2023-11-15 20:50:26 +0900

    add :: admin query user controller

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/admin/presentation/AdminController.kt
@@ -0,0 +1,23 @@
+package team.msg.domain.admin.presentation
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import team.msg.domain.admin.service.AdminService
+import team.msg.domain.user.presentation.data.response.UsersResponse
+
+@RestController
+@RequestMapping("/admin")
+class AdminController(
+    private val adminService: AdminService
+) {
+    @GetMapping
+    fun queryUser(@RequestParam keyword: String = ""): ResponseEntity<UsersResponse> {
+        val response = adminService.queryUsers(keyword)
+        return ResponseEntity.status(HttpStatus.OK).body(response)
+    }
+
+}
