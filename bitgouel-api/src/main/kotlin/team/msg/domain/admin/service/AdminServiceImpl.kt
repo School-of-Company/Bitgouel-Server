6135e9e1b987f1646c7b90baa0a456ece38ae2d2
KimTaeO <taeo0328@gmail.com>
2023-11-18 21:42:03 +0900

    add :: User List Service 주석

--- bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
@@ -10,6 +10,10 @@ import team.msg.domain.user.repository.UserRepository
 class AdminServiceImpl(
     private val userRepository: UserRepository
 ) : AdminService {
+    /**
+     * 유저를 전체 조회 및 이름으로 조회하는 비즈니스 로직입니다
+     * @param 유저를 검색하기 위한 keyword 및 페이징을 처리하기 위한 pageable
+     */
     override fun queryUsers(keyword: String, pageable: Pageable): UsersResponse {
         val users =
             if(keyword == "")
