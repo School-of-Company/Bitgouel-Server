ba6248e3f425d4dd5053701ddfebcb1fe06dbe40
KimTaeO <taeo0328@gmail.com>
2023-11-19 02:12:18 +0900

    add :: 주석에 @return 추가

--- bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
@@ -13,6 +13,7 @@ class AdminServiceImpl(
     /**
      * 유저를 전체 조회 및 이름으로 조회하는 비즈니스 로직입니다
      * @param 유저를 검색하기 위한 keyword 및 페이징을 처리하기 위한 pageable
+     * @return 페이징된 학생 정보를 담은 Dto
      */
     override fun queryUsers(keyword: String, pageable: Pageable): UsersResponse {
         val users =
