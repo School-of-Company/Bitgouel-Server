7aede7235dff0564d7f3579d500e3e6b2c1d5013
KimTaeO <taeo0328@gmail.com>
2023-11-18 21:20:01 +0900

    add :: 유저 조회에 이용되는 쿼리메소드

--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
@@ -1,5 +1,7 @@
 package team.msg.domain.user.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 import team.msg.domain.user.model.User
 import java.util.UUID
@@ -8,4 +10,7 @@ interface UserRepository : CrudRepository<User, UUID> {
     fun existsByEmail(email: String): Boolean
     fun existsByPhoneNumber(phoneNumber: String): Boolean
     fun findByEmail(email: String): User?
+
+    fun findByNameContaining(keyword: String, pageable: Pageable): Page<User>
+    fun findAll(pageable: Pageable): Page<User>
 }
