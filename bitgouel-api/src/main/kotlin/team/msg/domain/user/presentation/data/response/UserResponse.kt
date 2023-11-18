58fd697cb91b7474ecc012f6be010d707de79524
KimTaeO <taeo0328@gmail.com>
2023-11-18 21:20:43 +0900

    create User List Response

--- bitgouel-api/src/main/kotlin/team/msg/domain/user/presentation/data/response/UserResponse.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/user/presentation/data/response/UserResponse.kt
@@ -1,5 +1,7 @@
 package team.msg.domain.user.presentation.data.response
 
+import org.springframework.data.domain.Page
+import team.msg.common.enums.ApproveStatus
 import team.msg.domain.user.enums.Authority
 import team.msg.domain.user.model.User
 import java.util.*
@@ -17,6 +19,17 @@ data class UserResponse (
             authority = user.authority,
             organization = organization
         )
+
+        fun pageOf(users: Page<User>) = UsersResponse(
+            users.map {
+                AdminUserResponse(
+                    id = it.id,
+                    name = it.name,
+                    authority = it.authority,
+                    approveStatus = it.approveStatus
+                )
+            }
+        )
     }
 }
 
@@ -26,4 +39,16 @@ data class UserPageResponse (
     val phoneNumber: String,
     val authority: Authority,
     val organization: String
+)
+
+data class AdminUserResponse(
+    val id: UUID,
+    val name: String,
+    val authority: Authority,
+    val approveStatus: ApproveStatus
+)
+
+data class UsersResponse(
+    val users: Page<AdminUserResponse>
+
 )
