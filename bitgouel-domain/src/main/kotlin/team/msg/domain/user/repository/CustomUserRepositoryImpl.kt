5899a424d61ded7f64dd737237f7656a959d39a3
KimTaeO <taeo0328@gmail.com>
2023-11-19 22:16:52 +0900

    create :: query dsl을 사용하여 user 검색 및 조회

---
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/CustomUserRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package team.msg.domain.user.repository
+
+import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import team.msg.domain.user.enums.Authority
+import team.msg.domain.user.model.QUser.user
+import team.msg.domain.user.model.User
+import java.util.Objects.isNull
+
+class CustomUserRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : CustomUserRepository {
+    override fun query(keyword: String, authority: Authority, pageable: Pageable): Page<User> {
+        val users = queryFactory
+            .selectFrom(user)
+            .where(
+                nameLike(keyword),
+                authorityEq(authority)
+            )
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .orderBy(user.authority.asc())
+            .fetch()
+
+        return PageImpl(users)
+    }
+
+    private fun nameLike(keyword: String): BooleanExpression? =
+        if(keyword == "") null else user.name.like(keyword)
+
+    private fun authorityEq(authority: Authority): BooleanExpression? =
+        if(authority == Authority.ROLE_USER) null else user.authority.eq(authority)
+}
--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
@@ -6,11 +6,8 @@ import org.springframework.data.repository.CrudRepository
 import team.msg.domain.user.model.User
 import java.util.UUID
 
-interface UserRepository : CrudRepository<User, UUID> {
+interface UserRepository : CrudRepository<User, UUID>, CustomUserRepository {
     fun existsByEmail(email: String): Boolean
     fun existsByPhoneNumber(phoneNumber: String): Boolean
     fun findByEmail(email: String): User?
-
-    fun findByNameContaining(keyword: String, pageable: Pageable): Page<User>
-    fun findAll(pageable: Pageable): Page<User>
 }
