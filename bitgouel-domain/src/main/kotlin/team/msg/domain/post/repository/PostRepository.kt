c74dcf59483b466facc2a6c492906a555ce2ecfe
ani2689 <ojno7777@gmail.com>
2023-11-19 23:12:38 +0900

    update :: 페이징을 위한 jpa repository

--- bitgouel-domain/src/main/kotlin/team/msg/domain/post/repository/PostRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/post/repository/PostRepository.kt
@@ -1,7 +1,7 @@
 package team.msg.domain.post.repository
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import team.msg.domain.post.model.Post
 import java.util.UUID
 
-interface PostRepository : CrudRepository<Post, UUID>
+interface PostRepository : JpaRepository<Post, UUID>
