faac62de3704635484d9bd7cfa099272ccc98cf1
aesperer <piyrw9754@gmail.com>
2023-11-25 22:32:14 +0900

    docs :: student graduate job 설명 주석 추가

--- bitgouel-batch/src/main/kotlin/team/msg/job/student/GraduateStudentJobConfiguration.kt
+++ bitgouel-batch/src/main/kotlin/team/msg/job/student/GraduateStudentJobConfiguration.kt
@@ -6,17 +6,26 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import team.msg.common.util.ParamUtil
 
+/**
+ * parameter로 넘어온 period의 student를 graduate로 갱신하는 Job
+ * > 첫 실행 2024. 03. 02
+ * period 설정 기준 현재년도 - 2023
+ */
 @Configuration
 class GraduateStudentJobConfiguration(
     private val parameter: GraduateJobParameter
 ) {
 
     companion object {
-        const val CHUNK_SIZE: Int = 50
+        const val CHUNK_SIZE: Int = 50 // 변경될 수 있음
         const val JOB_NAME: String = "graduate_student_job"
         const val PARAMETER: String = JOB_NAME + "_parameter"
     }
 
+    /**
+     * GraduateStudentJobParamter 실행 커맨드 예시
+     * --JOB_NAME=graduate_student_job VERSION=1 DATE_TIME=2024/03/02-00:00:00-KST
+     */
     @Bean(PARAMETER)
     @JobScope
     fun parameter(
