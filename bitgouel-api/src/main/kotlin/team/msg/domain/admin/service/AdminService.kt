cfe261210ecf6fd14e0594984e22ec40470067b2
KimTaeO <taeo0328@gmail.com>
2023-11-27 14:21:12 +0900

    add :: approve user service

--- bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminService.kt
@@ -3,7 +3,9 @@ package team.msg.domain.admin.service
 import org.springframework.data.domain.Pageable
 import team.msg.domain.admin.presentation.data.request.QueryUsersRequest
 import team.msg.domain.user.presentation.data.response.UsersResponse
+import java.util.UUID
 
 interface AdminService {
-    fun queryUsers(request: QueryUsersRequest,pageable: Pageable): UsersResponse
+    fun queryUsers(request: QueryUsersRequest, pageable: Pageable): UsersResponse
+    fun approveUser(userId: UUID)
 }
--- bitgouel-api/src/main/kotlin/team/msg/domain/user/exception/UserAlreadyApprovedException.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/user/exception/UserAlreadyApprovedException.kt
@@ -3,7 +3,7 @@ package team.msg.domain.user.exception
 import team.msg.domain.user.exception.constant.UserErrorCode
 import team.msg.global.error.exception.BitgouelException
 
-class UserAlreadyApproved(
+class UserAlreadyApprovedException(
     message: String
 ) : BitgouelException(message, UserErrorCode.USER_ALREADY_APPROVED.status) {
 }
