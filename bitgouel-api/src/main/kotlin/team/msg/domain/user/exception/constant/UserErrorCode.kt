6e38927d521a529d5c7a72935a92c9512d80f8d7
KimTaeO <taeo0328@gmail.com>
2023-11-27 14:16:14 +0900

    create :: user already approved exception

--- bitgouel-api/src/main/kotlin/team/msg/domain/user/exception/constant/UserErrorCode.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/user/exception/constant/UserErrorCode.kt
@@ -3,5 +3,6 @@ package team.msg.domain.user.exception.constant
 enum class UserErrorCode(
     val status: Int
 ) {
-    USER_NOT_FOUND(404)
+    USER_NOT_FOUND(404),
+    USER_ALREADY_APPROVED(409)
 }
