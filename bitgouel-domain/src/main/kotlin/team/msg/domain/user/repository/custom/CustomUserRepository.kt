9a3941a3613f0180d45ed5dc2c63d2a328899230
ani2689 <ojno7777@gmail.com>
2023-12-01 09:23:58 +0900

    add :: User id로 name 조회 메서드 추가

--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/custom/CustomUserRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/custom/CustomUserRepository.kt
@@ -4,7 +4,9 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import team.msg.domain.user.enums.Authority
 import team.msg.domain.user.model.User
+import java.util.*
 
 interface CustomUserRepository {
     fun query(keyword: String, authority: Authority, pageable: Pageable): Page<User>
+    fun queryNameById(id: UUID): String?
 }
