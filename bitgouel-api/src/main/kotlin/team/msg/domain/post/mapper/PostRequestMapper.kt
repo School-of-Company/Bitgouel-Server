ffefb4074c71575b4483ed9b7827da9355cfa4e3
ani2689 <ojno7777@gmail.com>
2023-12-04 17:01:36 +0900

    add :: update post mapper

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapper.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapper.kt
@@ -1,9 +1,11 @@
 package team.msg.domain.post.mapper
 
-import team.msg.domain.post.enums.FeedType
 import team.msg.domain.post.presentation.data.request.CreatePostRequest
+import team.msg.domain.post.presentation.data.request.UpdatePostRequest
 import team.msg.domain.post.presentation.web.CreatePostWebRequest
+import team.msg.domain.post.presentation.web.UpdatePostWebRequest
 
 interface PostRequestMapper {
     fun createPostWebRequestToDto(webRequest: CreatePostWebRequest): CreatePostRequest
+    fun updatePostWebRequestToDto(webRequest: UpdatePostWebRequest): UpdatePostRequest
 }
