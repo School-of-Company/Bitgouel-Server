19c0b5f3b94137ecdc9aa2a99299982a5afcd620
ani2689 <ojno7777@gmail.com>
2023-12-04 17:03:41 +0900

    add :: update post mapper 구현

--- bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/post/mapper/PostRequestMapperImpl.kt
@@ -2,7 +2,9 @@ package team.msg.domain.post.mapper
 
 import org.springframework.stereotype.Component
 import team.msg.domain.post.presentation.data.request.CreatePostRequest
+import team.msg.domain.post.presentation.data.request.UpdatePostRequest
 import team.msg.domain.post.presentation.web.CreatePostWebRequest
+import team.msg.domain.post.presentation.web.UpdatePostWebRequest
 
 @Component
 class PostRequestMapperImpl : PostRequestMapper {
@@ -15,6 +17,17 @@ class PostRequestMapperImpl : PostRequestMapper {
             content = content,
             link = link?.map { it.url },
             feedType = feedType
+        )
+    }
+
+    /**
+     * post 수정 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
+    override fun updatePostWebRequestToDto(webRequest: UpdatePostWebRequest) = webRequest.run {
+        UpdatePostRequest(
+            title = title,
+            content = content,
+            link = link?.map { it.url }
         )
     }
 }
