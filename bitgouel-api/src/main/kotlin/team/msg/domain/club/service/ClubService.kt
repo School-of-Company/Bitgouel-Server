12b6da1bacdc75a178fb2845f4c91c4066d2b6ee
ani2689 <ojno7777@gmail.com>
2023-12-28 21:05:23 +0900

    add :: query student details method

--- bitgouel-api/src/main/kotlin/team/msg/domain/club/service/ClubService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/club/service/ClubService.kt
@@ -4,9 +4,12 @@ import team.msg.domain.club.presentation.data.response.ClubsResponse
 import team.msg.domain.club.presentation.data.response.ClubDetailsResponse
 import team.msg.domain.school.enums.HighSchool
 import team.msg.domain.student.presentation.data.response.AllStudentsResponse
+import team.msg.domain.student.presentation.data.response.StudentDetailsResponse
+import java.util.UUID
 
 interface ClubService {
     fun queryAllClubsService(highSchool: HighSchool): ClubsResponse
     fun queryClubDetailsService(id: Long): ClubDetailsResponse
     fun queryAllStudentsByClubId(id: Long): AllStudentsResponse
+    fun queryStudentDetails(id: Long, studentId: UUID): StudentDetailsResponse
 }
