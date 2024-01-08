69c929b184b937e88b2e7ff0b0a6352e5d1be5ef
JuuuuHong <juhang720@gmail.com>
2024-01-08 18:50:53 +0900

    update :: query club by id 접근권한 변경

--- bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
@@ -63,7 +63,8 @@ class SecurityConfig(
 
             // club
             .mvcMatchers(HttpMethod.GET, "/club").hasRole(ADMIN)
-            .mvcMatchers(HttpMethod.GET, "/club/{id}").hasAnyRole(STUDENT, ADMIN, PROFESSOR, COMPANY_INSTRUCTOR, BBOZZAK, TEACHER, GOVERNMENT)
+            .mvcMatchers(HttpMethod.GET, "/club/{id}").hasAnyRole(ADMIN)
+            .mvcMatchers(HttpMethod.GET, "/club/my").hasAnyRole(STUDENT, ADMIN, PROFESSOR, COMPANY_INSTRUCTOR, BBOZZAK, TEACHER, GOVERNMENT)
             .mvcMatchers(HttpMethod.GET, "/club/{id}/member").hasAnyRole(STUDENT, ADMIN, PROFESSOR, COMPANY_INSTRUCTOR, BBOZZAK, TEACHER, GOVERNMENT)
             .mvcMatchers(HttpMethod.GET, "/club/{id}/{student_id}").hasAnyRole(STUDENT, ADMIN, PROFESSOR, COMPANY_INSTRUCTOR, BBOZZAK, TEACHER, GOVERNMENT)
 
