2f85608fe991d6b2846491a8628012a3fb657b2d
aesperer <piyrw9754@gmail.com>
2024-01-09 21:26:57 +0900

    create :: withdraw student list api 이거까지 하나 남아있어서 해치우고 갑니다~

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/withdraw/presentation/WithdrawStudentController.kt
@@ -0,0 +1,20 @@
+package team.msg.domain.withdraw.presentation
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import team.msg.domain.withdraw.presentation.data.response.WithdrawStudentResponses
+import team.msg.domain.withdraw.service.WithdrawService
+
+@RestController
+class WithdrawStudentController(
+    private val withdrawService: WithdrawService
+) {
+
+    @GetMapping
+    fun queryWithdrawStudent(@RequestParam cohort: Int): ResponseEntity<WithdrawStudentResponses> {
+        val response = withdrawService.queryWithdrawStudent(cohort)
+        return ResponseEntity.ok(response)
+    }
+}
