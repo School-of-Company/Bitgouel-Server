ea454989b9b287afcd4a57685b8fa3f9d8122bba
aesperer <piyrw9754@gmail.com>
2024-01-09 21:04:54 +0900

    create :: inquiry reply, update api

--- bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/mapper/InquiryMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/mapper/InquiryMapperImpl.kt
@@ -1,8 +1,12 @@
 package team.msg.domain.inquiry.mapper
 
 import org.springframework.stereotype.Component
+import team.msg.domain.inquiry.presentation.request.CreateInquiryAnswerRequest
 import team.msg.domain.inquiry.presentation.request.CreateInquiryRequest
+import team.msg.domain.inquiry.presentation.request.UpdateInquiryRequest
+import team.msg.domain.inquiry.presentation.web.CreateInquiryAnswerWebRequest
 import team.msg.domain.inquiry.presentation.web.CreateInquiryWebRequest
+import team.msg.domain.inquiry.presentation.web.UpdateInquiryWebRequest
 
 @Component
 class InquiryMapperImpl : InquiryMapper {
@@ -11,6 +15,16 @@ class InquiryMapperImpl : InquiryMapper {
         CreateInquiryRequest(
             question = webRequest.question,
             questionDetail = webRequest.questionDetail
+        )
+
+    override fun updateInquiryWebRequestToDto(webRequest: UpdateInquiryWebRequest): UpdateInquiryRequest =
+        UpdateInquiryRequest(
+            question = webRequest.question,
+            questionDetail = webRequest.questionDetail
+        )
+    override fun createInquiryAnswerWebRequestToDto(webRequest: CreateInquiryAnswerWebRequest): CreateInquiryAnswerRequest =
+        CreateInquiryAnswerRequest(
+            answer = webRequest.answer
         )
 
 }
