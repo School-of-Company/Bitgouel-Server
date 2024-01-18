2416600fb1990dcf7137dce22cd5ba11dc643644
ani2689 <ojno7777@gmail.com>
2024-01-19 01:16:48 +0900

    bug :: param이 null이 되지 않도록 dto에 request 받기

--- bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/presentation/InquiryController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/presentation/InquiryController.kt
@@ -10,14 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import team.msg.domain.inquiry.enums.AnswerStatus
 import team.msg.domain.inquiry.mapper.InquiryMapper
 import team.msg.domain.inquiry.presentation.response.InquiryDetailResponse
 import team.msg.domain.inquiry.presentation.response.InquiryResponses
 import team.msg.domain.inquiry.presentation.web.CreateInquiryAnswerWebRequest
 import team.msg.domain.inquiry.presentation.web.CreateInquiryWebRequest
+import team.msg.domain.inquiry.presentation.request.QueryAllInquiresWebRequest
 import team.msg.domain.inquiry.presentation.web.UpdateInquiryWebRequest
 import team.msg.domain.inquiry.service.InquiryService
 import java.util.UUID
@@ -43,9 +42,9 @@ class InquiryController(
     }
 
     @GetMapping("/all")
-    fun queryAllInquires(@RequestParam answerStatus: AnswerStatus? = null,
-                         @RequestParam keyword: String = ""): ResponseEntity<InquiryResponses> {
-        val response = inquiryService.queryAllInquiries(answerStatus,keyword)
+    fun queryAllInquires(webRequest: QueryAllInquiresWebRequest): ResponseEntity<InquiryResponses> {
+        val request = inquiryMapper.queryAllInquiresWebRequestToDto(webRequest)
+        val response = inquiryService.queryAllInquiries(request)
         return ResponseEntity.ok(response)
     }
 
