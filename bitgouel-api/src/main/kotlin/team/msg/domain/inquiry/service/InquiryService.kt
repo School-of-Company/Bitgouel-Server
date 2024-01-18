b70a094d7eb4fd945a8e9ee939931091886527b2
ani2689 <ojno7777@gmail.com>
2024-01-19 01:15:54 +0900

    update :: dto를 통해 매개변수 받기

--- bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/service/InquiryService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/inquiry/service/InquiryService.kt
@@ -1,17 +1,17 @@
 package team.msg.domain.inquiry.service
 
-import team.msg.domain.inquiry.enums.AnswerStatus
 import team.msg.domain.inquiry.presentation.request.CreateInquiryAnswerRequest
 import team.msg.domain.inquiry.presentation.request.CreateInquiryRequest
 import team.msg.domain.inquiry.presentation.request.UpdateInquiryRequest
 import team.msg.domain.inquiry.presentation.response.InquiryDetailResponse
 import team.msg.domain.inquiry.presentation.response.InquiryResponses
+import team.msg.domain.inquiry.presentation.web.QueryAllInquiresRequest
 import java.util.UUID
 
 interface InquiryService {
     fun createInquiry(request: CreateInquiryRequest)
     fun queryMyInquiries(): InquiryResponses
-    fun queryAllInquiries(answerStatus: AnswerStatus?, keyword: String): InquiryResponses
+    fun queryAllInquiries(request: QueryAllInquiresRequest): InquiryResponses
     fun queryInquiryDetail(id: UUID): InquiryDetailResponse
     fun deleteInquiry(id: UUID)
     fun rejectInquiry(id: UUID)
