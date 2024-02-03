bb746086e8cdd9dd5b51fed7bfb72c70cfa7112a
JuuuuHong <juhang720@gmail.com>
2024-02-03 20:58:40 +0900

    test :: query faq details service

--- bitgouel-api/src/test/kotlin/team/msg/domain/faq/service/FaqServiceImplTest.kt
+++ bitgouel-api/src/test/kotlin/team/msg/domain/faq/service/FaqServiceImplTest.kt
@@ -8,16 +8,19 @@ import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.springframework.data.repository.findByIdOrNull
 import team.msg.common.util.UserUtil
 import team.msg.domain.admin.exception.AdminNotFoundException
 import team.msg.domain.admin.model.Admin
 import team.msg.domain.admin.repository.AdminRepository
 import team.msg.domain.faq.model.Faq
 import team.msg.domain.faq.presentation.data.request.CreateFaqRequest
+import team.msg.domain.faq.presentation.data.response.FaqDetailsResponse
 import team.msg.domain.faq.presentation.data.response.FaqResponse
 import team.msg.domain.faq.presentation.data.response.FaqsResponse
 import team.msg.domain.faq.repository.FaqRepository
 import team.msg.domain.user.model.User
+import java.util.UUID
 
 class FaqServiceImplTest : BehaviorSpec({
 
@@ -64,17 +67,44 @@ class FaqServiceImplTest : BehaviorSpec({
     }
 
     // queryAllFaqs 테스트 코드
+//    Given("FAQ 가 주어질 때") {
+//        val faq = fixture<Faq>()
+//        val faqResponse = fixture<FaqResponse> {
+//            property(FaqResponse::id) { faq.id }
+//            property(FaqResponse::question) { faq.question }
+//        }
+//        val faqResponseList = listOf(faqResponse)
+//        val response = fixture<FaqsResponse> {
+//            property(FaqsResponse::faqs) { faqResponseList }
+//        }
+//
+//        every { faqRepository.findAll() } returns listOf(faq)
+//
+//        When("FAQ 전체 조회 요청을 하면") {
+//            val result = faqServiceImpl.queryAllFaqs()
+//
+//            Then("result와 response가 같아야 한다.") {
+//                result shouldBe response
+//            }
+//        }
+//    }
+
+    // queryFaqDetails 테스트 코드
     Given("FAQ 가 주어질 때") {
-        val faq = fixture<Faq>()
-        val faqResponse = fixture<List<FaqResponse>>()
-        val response = fixture<FaqsResponse> {
-            property(FaqsResponse::faqs) { faqResponse }
+        val faqId = 1L
+        val faq = fixture<Faq> {
+            property(Faq::id) { faqId }
+        }
+        val response = fixture<FaqDetailsResponse> {
+            property(FaqDetailsResponse::id) { faqId }
+            property(FaqDetailsResponse::question) { faq.question }
+            property(FaqDetailsResponse::answer) { faq.answer }
         }
 
-        every { faqRepository.findAll() } returns listOf(faq)
+        every { faqRepository.findByIdOrNull(faqId) } returns faq
 
         When("FAQ 전체 조회 요청을 하면") {
-            val result = faqServiceImpl.queryAllFaqs()
+            val result = faqServiceImpl.queryFaqDetails(faqId)
 
             Then("result와 response가 같아야 한다.") {
                 result shouldBe response
