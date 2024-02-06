44bdbd281a6d431803fa403fee6067621caaff3f
JuuuuHong <juhang720@gmail.com>
2024-02-06 21:59:02 +0900

    test :: teacher query certifications service

--- bitgouel-api/src/test/kotlin/team/msg/domain/certification/service/CertificationImplTest.kt
+++ bitgouel-api/src/test/kotlin/team/msg/domain/certification/service/CertificationImplTest.kt
@@ -11,12 +11,16 @@ import io.mockk.verify
 import team.msg.common.util.UserUtil
 import team.msg.domain.certifiacation.model.Certification
 import team.msg.domain.certifiacation.repository.CertificationRepository
+import team.msg.domain.certification.exception.ForbiddenCertificationException
 import team.msg.domain.certification.presentation.data.request.CreateCertificationRequest
 import team.msg.domain.certification.presentation.data.response.CertificationResponse
 import team.msg.domain.certification.presentation.data.response.CertificationsResponse
+import team.msg.domain.club.model.Club
 import team.msg.domain.student.exception.StudentNotFoundException
 import team.msg.domain.student.model.Student
 import team.msg.domain.student.repository.StudentRepository
+import team.msg.domain.teacher.exception.TeacherNotFoundException
+import team.msg.domain.teacher.model.Teacher
 import team.msg.domain.teacher.repository.TeacherRepository
 import team.msg.domain.user.model.User
 import java.time.LocalDate
@@ -70,17 +74,21 @@ class CertificationImplTest : BehaviorSpec ({
 
     // queryCertifications 테스트 코드
     Given("Certification 이 주어질 때") {
+        val certificationId = UUID.randomUUID()
+        val certificationName = "자격증"
+        val certificationAcquisitionDate = LocalDate.MAX
+
         val user = fixture<User>()
         val student = fixture<Student>()
         val certification = fixture<Certification> {
-            property(Certification::id) { UUID.randomUUID() }
-            property(Certification::name) { "자격증" }
-            property(Certification::acquisitionDate) { LocalDate.MAX }
+            property(Certification::id) { certificationId }
+            property(Certification::name) { certificationName }
+            property(Certification::acquisitionDate) { certificationAcquisitionDate }
         }
         val certificationResponse = fixture<CertificationResponse> {
-            property(CertificationResponse::id) { certification.id }
-            property(CertificationResponse::name) { "자격증" }
-            property(CertificationResponse::acquisitionDate) { LocalDate.MAX }
+            property(CertificationResponse::id) { certificationId }
+            property(CertificationResponse::name) { certificationName }
+            property(CertificationResponse::acquisitionDate) { certificationAcquisitionDate }
         }
         val response = fixture<CertificationsResponse> {
             property(CertificationsResponse::certifications) { listOf(certificationResponse) }
@@ -104,6 +112,80 @@ class CertificationImplTest : BehaviorSpec ({
             Then("StudentNotFoundException 가 터져야 한다.") {
                 shouldThrow<StudentNotFoundException> {
                     certificationServiceImpl.queryCertifications()
+                }
+            }
+        }
+    }
+
+    // queryCertifications 테스트 코드
+    Given("Certification 이 주어질 때") {
+        val certificationId = UUID.randomUUID()
+        val certificationName = "자격증"
+        val certificationAcquisitionDate = LocalDate.MAX
+        val request = UUID.randomUUID()
+
+        val club = fixture<Club>()
+        val user = fixture<User>()
+        val teacher = fixture<Teacher> {
+            property(Teacher::club) { club }
+        }
+        val invalidTeacher = fixture<Teacher>()
+        val student = fixture<Student> {
+            property(Student::club) { club }
+        }
+        val certification = fixture<Certification> {
+            property(Certification::id) { certificationId }
+            property(Certification::name) { certificationName }
+            property(Certification::acquisitionDate) { certificationAcquisitionDate }
+        }
+        val certificationResponse = fixture<CertificationResponse> {
+            property(CertificationResponse::id) { certificationId }
+            property(CertificationResponse::name) { certificationName }
+            property(CertificationResponse::acquisitionDate) { certificationAcquisitionDate }
+        }
+        val response = fixture<CertificationsResponse> {
+            property(CertificationsResponse::certifications) { listOf(certificationResponse) }
+        }
+
+        every { userUtil.queryCurrentUser() } returns user
+        every { teacherRepository.findByUser(user) } returns teacher
+        every { studentRepository.findStudentById(request) } returns student
+        every { certificationRepository.findAllByStudentIdOrderByAcquisitionDateDesc(request) } returns listOf(certification)
+
+        When("자격증 전체 조회 요청을 하면") {
+            val result = certificationServiceImpl.queryCertifications(request)
+
+            Then("result와 response가 같아야 한다.") {
+                result shouldBe response
+            }
+        }
+
+        When("현재 유저가 선생님이 아니라면") {
+            every { teacherRepository.findByUser(user) } returns null
+
+            Then("TeacherNotFoundException 가 터져야 한다.") {
+                shouldThrow<TeacherNotFoundException> {
+                    certificationServiceImpl.queryCertifications(request)
+                }
+            }
+        }
+
+        When("request 에 맞는 학생이 없다면") {
+            every { studentRepository.findStudentById(request) } returns null
+
+            Then("StudentNotFoundException 가 터져야 한다.") {
+                shouldThrow<StudentNotFoundException> {
+                    certificationServiceImpl.queryCertifications(request)
+                }
+            }
+        }
+
+        When("선생님 소속 동아리가 아닌 학생의 자격증을 조회한다면") {
+            every { teacherRepository.findByUser(user) } returns invalidTeacher
+
+            Then("ForbiddenCertificationException 가 터져야 한다.") {
+                shouldThrow<ForbiddenCertificationException> {
+                    certificationServiceImpl.queryCertifications(request)
                 }
             }
         }
