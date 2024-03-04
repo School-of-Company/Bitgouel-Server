1dde227a812753ba167d95f2abb4404ead40fbf0
JuuuuHong <juhang720@gmail.com>
2024-03-04 19:54:49 +0900

    test :: query my inquiries service

--- bitgouel-api/src/test/kotlin/team/msg/domain/inquiry/service/InquiryServiceImplTest.kt
+++ bitgouel-api/src/test/kotlin/team/msg/domain/inquiry/service/InquiryServiceImplTest.kt
@@ -3,12 +3,15 @@ package team.msg.domain.inquiry.service
 import com.appmattus.kotlinfixture.kotlinFixture
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import team.msg.common.util.UserUtil
 import team.msg.domain.inquiry.model.Inquiry
 import team.msg.domain.inquiry.presentation.request.CreateInquiryRequest
+import team.msg.domain.inquiry.presentation.response.InquiryResponse
+import team.msg.domain.inquiry.presentation.response.InquiryResponses
 import team.msg.domain.inquiry.repository.InquiryAnswerRepository
 import team.msg.domain.inquiry.repository.InquiryRepository
 import team.msg.domain.user.model.User
@@ -41,6 +44,34 @@ class InquiryServiceImplTest : BehaviorSpec ({
 
             Then("Inquiry 이 저장이 되어야 한다.") {
                 verify(exactly = 1) { inquiryRepository.save(any()) }
+            }
+        }
+    }
+
+    // queryMyInquiries 테스트 코드
+    Given("Inquiry 가 주어질 때") {
+        val user = fixture<User>()
+        val inquiry = fixture<Inquiry>()
+        val inquiryResponse = fixture<InquiryResponse> {
+            property(InquiryResponse::id) { inquiry.id }
+            property(InquiryResponse::question) { inquiry.question }
+            property(InquiryResponse::userId) { inquiry.user.id }
+            property(InquiryResponse::username) { inquiry.user.name }
+            property(InquiryResponse::createdAt) { inquiry.createdAt.toLocalDate() }
+            property(InquiryResponse::answerStatus) { inquiry.answerStatus }
+        }
+        val response = fixture<InquiryResponses> {
+            property(InquiryResponses::inquiries) { listOf(inquiryResponse) }
+        }
+
+        every { userUtil.queryCurrentUser() } returns user
+        every { inquiryRepository.findAllByUser(user) } returns listOf(inquiry)
+
+        When("자격증 전체 조회 요청을 하면") {
+            val result = inquiryServiceImpl.queryMyInquiries()
+
+            Then("result와 response가 같아야 한다.") {
+                result shouldBe response
             }
         }
     }
