6a5042e952e6e5107e78608c2397f5f3c858036c
KimTaeO <taeo0328@gmail.com>
2024-03-08 11:57:18 +0900

    add :: query annotation

--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package team.msg.domain.user.repository
 
 import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import team.msg.domain.user.model.User
 import team.msg.domain.user.repository.custom.CustomUserRepository
@@ -12,5 +13,6 @@ interface UserRepository : CrudRepository<User, UUID>, CustomUserRepository {
     fun findByEmail(email: String): User?
     fun findByIdIn(ids: List<UUID>): List<User>
     @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("delete from User u where u.id in :ids")
     fun deleteByIdIn(ids: List<UUID>)
 }
