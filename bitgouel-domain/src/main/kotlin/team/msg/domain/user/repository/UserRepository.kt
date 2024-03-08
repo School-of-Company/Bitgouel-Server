d4d4efa3f3fe34314f41ee9d408a57646f9f75c1
KimTaeO <taeo0328@gmail.com>
2024-03-08 10:03:36 +0900

    add :: delete bulk query

--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package team.msg.domain.user.repository
 
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.repository.CrudRepository
 import team.msg.domain.user.model.User
 import team.msg.domain.user.repository.custom.CustomUserRepository
@@ -10,4 +11,6 @@ interface UserRepository : CrudRepository<User, UUID>, CustomUserRepository {
     fun existsByPhoneNumber(phoneNumber: String): Boolean
     fun findByEmail(email: String): User?
     fun findByIdIn(ids: List<UUID>): List<User>
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    fun deleteByIdIn(ids: List<UUID>)
 }
