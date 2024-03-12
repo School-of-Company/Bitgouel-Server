ed6184c948f2a7de9150402ca541a26e532079b5
ani2689 <ojno7777@gmail.com>
2024-03-12 23:24:02 +0900

    create :: 강의 시간 조회 query

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/LectureDateRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/LectureDateRepository.kt
@@ -1,7 +1,10 @@
 package team.msg.domain.lecture.repository
 
 import org.springframework.data.repository.CrudRepository
+import team.msg.domain.lecture.model.Lecture
 import team.msg.domain.lecture.model.LectureDate
 import java.util.*
 
-interface LectureDateRepository : CrudRepository<LectureDate,UUID>
+interface LectureDateRepository : CrudRepository<LectureDate,UUID> {
+    fun findAllByLecture(lecture: Lecture): List<LectureDate>
+}
