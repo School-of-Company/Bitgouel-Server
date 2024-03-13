1d9d049bbfeb80c8609f5a0cf20bc996f56127b5
JuuuuHong <juhang720@gmail.com>
2024-03-13 17:49:00 +0900

    update :: query certifications service & security config

--- bitgouel-api/src/main/kotlin/team/msg/domain/certification/service/CertificationServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/certification/service/CertificationServiceImpl.kt
@@ -4,6 +4,12 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.msg.common.util.UserUtil
+import team.msg.domain.admin.exception.AdminNotFoundException
+import team.msg.domain.admin.model.Admin
+import team.msg.domain.admin.repository.AdminRepository
+import team.msg.domain.bbozzak.exception.BbozzakNotFoundException
+import team.msg.domain.bbozzak.model.Bbozzak
+import team.msg.domain.bbozzak.repository.BbozzakRepository
 import team.msg.domain.certifiacation.model.Certification
 import team.msg.domain.certifiacation.repository.CertificationRepository
 import team.msg.domain.certification.exception.CertificationNotFoundException
@@ -12,6 +18,15 @@ import team.msg.domain.certification.presentation.data.request.CreateCertificati
 import team.msg.domain.certification.presentation.data.request.UpdateCertificationRequest
 import team.msg.domain.certification.presentation.data.response.CertificationResponse
 import team.msg.domain.certification.presentation.data.response.CertificationsResponse
+import team.msg.domain.company.exception.CompanyNotFoundException
+import team.msg.domain.company.model.CompanyInstructor
+import team.msg.domain.company.repository.CompanyInstructorRepository
+import team.msg.domain.government.GovernmentNotFoundException
+import team.msg.domain.government.model.Government
+import team.msg.domain.government.repository.GovernmentRepository
+import team.msg.domain.professor.exception.ProfessorNotFoundException
+import team.msg.domain.professor.model.Professor
+import team.msg.domain.professor.repository.ProfessorRepository
 import team.msg.domain.student.exception.StudentNotFoundException
 import team.msg.domain.student.model.Student
 import team.msg.domain.student.repository.StudentRepository
@@ -19,6 +34,7 @@ import team.msg.domain.teacher.exception.TeacherNotFoundException
 import team.msg.domain.teacher.model.Teacher
 import team.msg.domain.teacher.repository.TeacherRepository
 import team.msg.domain.user.model.User
+import team.msg.global.exception.InvalidRoleException
 import java.util.*
 
 @Service
@@ -26,7 +42,12 @@ class CertificationServiceImpl(
     private val certificationRepository: CertificationRepository,
     private val studentRepository: StudentRepository,
     private val userUtil: UserUtil,
-    private val teacherRepository: TeacherRepository
+    private val teacherRepository: TeacherRepository,
+    private val bbozzakRepository: BbozzakRepository,
+    private val professorRepository: ProfessorRepository,
+    private val companyInstructorRepository: CompanyInstructorRepository,
+    private val governmentRepository: GovernmentRepository,
+    private val adminRepository: AdminRepository
 ) : CertificationService {
 
     /**
@@ -76,12 +97,23 @@ class CertificationServiceImpl(
     override fun queryCertifications(studentId: UUID): CertificationsResponse {
         val user = userUtil.queryCurrentUser()
 
-        val teacher = teacherRepository findByUser user
+        val entity = userUtil.getAuthorityEntityAndOrganization(user).first
+
+        val club = when(entity) {
+            is Student -> findStudentByUser(user).club
+            is Teacher -> findTeacherByUser(user).club
+            is Bbozzak -> findBbozzakByUser(user).club
+            is Professor -> findProfessorByUser(user).club
+            is CompanyInstructor -> findCompanyInstructorByUser(user).club
+            is Government -> findGovernmentByUser(user).club
+            is Admin -> {}
+            else ->  throw InvalidRoleException("유효하지 않은 권한입니다. info : [ userAuthority = ${user.authority} ]")
+        }
 
         val student = studentRepository findStudentById studentId
 
-        if (student.club != teacher.club)
-            throw ForbiddenCertificationException("자격증을 조회할 권한이 없습니다. info : [ club = ${teacher.club} ]")
+        if (student.club != club && entity !is Admin)
+            throw ForbiddenCertificationException("자격증을 조회할 권한이 없습니다. info : [ club = $club ]")
 
         val certifications = certificationRepository findAllByStudentIdOrderByAcquisitionDateDesc studentId
 
@@ -134,4 +166,25 @@ class CertificationServiceImpl(
     private infix fun TeacherRepository.findByUser(user: User): Teacher =
         this.findByUser(user)
             ?: throw TeacherNotFoundException("존재하지 않는 선생님입니다. info : [ userId = ${user.id} ]")
+
+    private fun findStudentByUser(user: User) = studentRepository.findByUser(user)
+        ?: throw StudentNotFoundException("학생을 찾을 수 없습니다. info : [ userId = ${user.id} ]")
+
+    private fun findTeacherByUser(user: User) = teacherRepository.findByUser(user)
+        ?: throw TeacherNotFoundException("취업 동아리 선생님을 찾을 수 없습니다. info : [ userId = ${user.id} ]")
+
+    private fun findBbozzakByUser(user: User) = bbozzakRepository.findByUser(user)
+        ?: throw BbozzakNotFoundException("뽀짝 선생님을 찾을 수 없습니다.  info : [ userId = ${user.id} ]")
+
+    private fun findProfessorByUser(user: User) = professorRepository.findByUser(user)
+        ?: throw ProfessorNotFoundException("대학 교수를 찾을 수 없습니다. info : [ userId = ${user.id} ]")
+
+    private fun findCompanyInstructorByUser(user: User) = companyInstructorRepository.findByUser(user)
+        ?: throw CompanyNotFoundException("기업 강사를 찾을 수 없습니다. info : [ userId = ${user.id} ]")
+
+    private fun findGovernmentByUser(user: User) = governmentRepository.findByUser(user)
+        ?: throw GovernmentNotFoundException("유관기관을 찾을 수 없습니다. info : [ userId = ${user.id} ]")
+
+    private fun findAdminByUser(user: User) = adminRepository.findByUser(user)
+        ?: throw AdminNotFoundException("관리자를 찾을 수 없습니다. info : [ userId = ${user.id} ]")
 }
