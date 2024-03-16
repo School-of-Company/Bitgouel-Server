7b18bd18072895828e0615b304e5467252a50d7a
KimTaeO <taeo0328@gmail.com>
2024-03-16 22:47:00 +0900

    add :: division name property

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/enums/Division.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/enums/Division.kt
@@ -7,10 +7,12 @@ package team.msg.domain.lecture.enums
  * AI_CONVERGENCE AI 융복합
  * CULTURAL_INDUSTRY 문화산업
  */
-enum class Division {
-    AUTOMOBILE_INDUSTRY,
-    ENERGY_INDUSTRY,
-    MEDICAL_HEALTHCARE,
-    AI_CONVERGENCE,
-    CULTURAL_INDUSTRY,
+enum class Division(
+    val divisionName: String
+) {
+    AUTOMOBILE_INDUSTRY("자동차 산업"),
+    ENERGY_INDUSTRY("에너지 산업"),
+    MEDICAL_HEALTHCARE("의료 헬스 케어"),
+    AI_CONVERGENCE("AI 융복합"),
+    CULTURAL_INDUSTRY("문화 산업"),
 }
