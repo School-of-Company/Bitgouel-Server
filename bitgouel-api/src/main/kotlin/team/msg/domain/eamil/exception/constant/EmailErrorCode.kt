c91985b166d79f249d58bb0d8331a51df6ee2624
ani2689 <ojno7777@gmail.com>
2024-03-17 02:30:32 +0900

    create :: 이메일 발송 예외

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/eamil/exception/constant/EmailErrorCode.kt
@@ -0,0 +1,8 @@
+package team.msg.domain.eamil.exception.constant
+
+enum class EmailErrorCode(
+    val status: Int
+) {
+    ALREADY_AUTHENTICATED_EMAIL(409),
+    TOO_MANY_EMAIL_AUTHENTICATION_REQUEST(429)
+}
