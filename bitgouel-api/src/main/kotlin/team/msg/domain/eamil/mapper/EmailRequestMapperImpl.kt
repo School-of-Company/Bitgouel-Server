ab260546e058960b7d5d16b85745086549cf370c
ani2689 <ojno7777@gmail.com>
2024-03-17 04:22:27 +0900

    update :: 주석 추가

--- bitgouel-api/src/main/kotlin/team/msg/domain/eamil/mapper/EmailRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/eamil/mapper/EmailRequestMapperImpl.kt
@@ -6,6 +6,9 @@ import team.msg.domain.eamil.presentation.web.SendAuthenticationEmailWebRequest
 
 @Component
 class EmailRequestMapperImpl : EmailRequestMapper {
+    /**
+     * 인증 email 전송 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
     override fun sendEmailWebRequestToDto(webRequest: SendAuthenticationEmailWebRequest): SendAuthenticationEmailRequestData =
         SendAuthenticationEmailRequestData(
             email = webRequest.email
