673859374ad0ad06ec3be39869bc62eacf7095e8
ani2689 <ojno7777@gmail.com>
2024-03-16 21:31:45 +0900

    create :: email redis entity

---
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/email/model/EmailAuthentication.kt
@@ -0,0 +1,12 @@
+package team.msg.domain.email.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash("email_authentication", timeToLive = 60 * 5)
+data class EmailAuthentication(
+    @Id
+    val email: String,
+    val code: String,
+    val authentication: Boolean
+)
