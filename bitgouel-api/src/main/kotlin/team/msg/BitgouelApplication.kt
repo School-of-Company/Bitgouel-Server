9c29d7071170f68c46f0547a2b75af344dfe9ebb
ani2689 <ojno7777@gmail.com>
2024-03-17 21:27:46 +0900

    delete :: @ConfigurationPropertiesScan 삭제

--- bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
+++ bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
@@ -1,11 +1,9 @@
 package team.msg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(scanBasePackages = ["team.msg"])
-@ConfigurationPropertiesScan
 class BitgouelApplication
 
 fun main(args: Array<String>) {
