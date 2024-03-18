b3b08039cc588634e0d9ed52dc081a98afbb86ae
ani2689 <ojno7777@gmail.com>
2024-03-18 20:13:50 +0900

    create :: 이메일 인증 여부 web data & mapper

--- bitgouel-api/src/main/kotlin/team/msg/domain/email/mapper/EmailRequestMapper.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/email/mapper/EmailRequestMapper.kt
@@ -1,8 +1,11 @@
 package team.msg.domain.email.mapper
 
+import team.msg.domain.email.presentation.data.request.CheckEmailAuthenticationRequestData
 import team.msg.domain.email.presentation.data.request.SendAuthenticationEmailRequestData
+import team.msg.domain.email.presentation.web.CheckAuthenticationEmailWebRequest
 import team.msg.domain.email.presentation.web.SendAuthenticationEmailWebRequest
 
 interface EmailRequestMapper {
     fun sendEmailWebRequestToDto(webRequest: SendAuthenticationEmailWebRequest): SendAuthenticationEmailRequestData
+    fun checkEmailWebRequestToDto(webRequest: CheckAuthenticationEmailWebRequest): CheckEmailAuthenticationRequestData
 }
--- bitgouel-api/src/main/kotlin/team/msg/domain/email/mapper/EmailRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/email/mapper/EmailRequestMapperImpl.kt
@@ -1,7 +1,9 @@
 package team.msg.domain.email.mapper
 
 import org.springframework.stereotype.Component
+import team.msg.domain.email.presentation.data.request.CheckEmailAuthenticationRequestData
 import team.msg.domain.email.presentation.data.request.SendAuthenticationEmailRequestData
+import team.msg.domain.email.presentation.web.CheckAuthenticationEmailWebRequest
 import team.msg.domain.email.presentation.web.SendAuthenticationEmailWebRequest
 
 @Component
@@ -11,6 +13,14 @@ class EmailRequestMapperImpl : EmailRequestMapper {
      */
     override fun sendEmailWebRequestToDto(webRequest: SendAuthenticationEmailWebRequest): SendAuthenticationEmailRequestData =
         SendAuthenticationEmailRequestData(
+            email = webRequest.email
+        )
+
+    /**
+     * 인증 이메일 여부 조회 Web Request 를 애플리케이션 영역에서 사용될 Dto 로 매핑합니다.
+     */
+    override fun checkEmailWebRequestToDto(webRequest: CheckAuthenticationEmailWebRequest): CheckEmailAuthenticationRequestData =
+        CheckEmailAuthenticationRequestData(
             email = webRequest.email
         )
 }
