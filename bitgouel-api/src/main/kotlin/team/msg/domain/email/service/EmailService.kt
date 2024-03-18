54885f06f440014a63787ba725da97782687b9b2
ani2689 <ojno7777@gmail.com>
2024-03-18 20:14:01 +0900

    create :: 이메일 인증 여부 service

--- bitgouel-api/src/main/kotlin/team/msg/domain/email/service/EmailService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/email/service/EmailService.kt
@@ -1,8 +1,11 @@
 package team.msg.domain.email.service
 
+import team.msg.domain.email.presentation.data.request.CheckEmailAuthenticationRequestData
 import team.msg.domain.email.presentation.data.request.SendAuthenticationEmailRequestData
+import team.msg.domain.email.presentation.data.response.CheckEmailAuthenticationResponseData
 
 interface EmailService {
     fun sendAuthenticationEmail(request: SendAuthenticationEmailRequestData)
     fun emailAuthentication(email: String, code: String)
+    fun checkEmailAuthentication(request: CheckEmailAuthenticationRequestData): CheckEmailAuthenticationResponseData
 }
