87bfbd02eeaf6b3fd3d4c60733b6f71c2d6b008a
JuuuuHong <juhang720@gmail.com>
2024-04-20 17:19:33 +0900

    add :: prometheus dependency 추가 및 url 접근 허용

--- bitgouel-api/build.gradle.kts
+++ bitgouel-api/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-actuator")
     }
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation(project(":bitgouel-domain"))
 }
 
