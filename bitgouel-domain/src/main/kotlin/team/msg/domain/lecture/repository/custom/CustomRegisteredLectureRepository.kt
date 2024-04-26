4688124b34607e6ae277dadc0075e4dd67a3a554
ani2689 <ojno7777@gmail.com>
2024-04-26 15:55:54 +0900

    add :: 학생이 신청한 강의 조회

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/CustomRegisteredLectureRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/CustomRegisteredLectureRepository.kt
@@ -1,8 +1,10 @@
 package team.msg.domain.lecture.repository.custom
 
+import team.msg.domain.lecture.model.Lecture
 import java.util.*
 
 interface CustomRegisteredLectureRepository {
     fun deleteAllByStudentId(studentId: UUID)
     fun existsOne(studentId: UUID,lectureId: UUID): Boolean
+    fun findLecturesByStudentId(studentId: UUID): List<Lecture>
 }
--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/impl/CustomRegisteredLectureRepositoryImpl.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/impl/CustomRegisteredLectureRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package team.msg.domain.lecture.repository.custom.impl
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import team.msg.domain.lecture.model.Lecture
 import team.msg.domain.lecture.model.QLecture.lecture
 import team.msg.domain.lecture.model.QRegisteredLecture.registeredLecture
 import team.msg.domain.lecture.repository.custom.CustomRegisteredLectureRepository
 import team.msg.domain.student.model.QStudent.student
+import team.msg.domain.user.model.QUser
 import java.util.*
 
 class CustomRegisteredLectureRepositoryImpl(
@@ -27,4 +29,13 @@ class CustomRegisteredLectureRepositoryImpl(
 
         return fetchOne != null
     }
+
+    override fun findLecturesByStudentId(studentId: UUID): List<Lecture> =
+        queryFactory.select(lecture)
+            .leftJoin(registeredLecture.lecture, lecture)
+            .from(registeredLecture)
+            .where(
+                student.id.eq(studentId)
+            )
+            .fetch()
 }
