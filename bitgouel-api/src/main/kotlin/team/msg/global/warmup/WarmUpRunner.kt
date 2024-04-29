93b2fd67641b499f0d2a2ce57a9835a505ecea60
JuuuuHong <juhang720@gmail.com>
2024-04-29 11:06:25 +0900

    update :: 도메인별로 client 나누기

--- bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
@@ -3,15 +3,15 @@ package team.msg.global.warmup
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
-import team.msg.thirdparty.feign.client.WarmUpClient
+import team.msg.thirdparty.feign.client.FaqClient
 
 @Component
 class WarmUpRunner(
-    private val warmUpClient: WarmUpClient
+    private val faqClient: FaqClient
 ): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         runCatching {
-            warmUpClient.queryFaq()
+            faqClient.queryFaq()
         }.onFailure {  }
     }
 }
