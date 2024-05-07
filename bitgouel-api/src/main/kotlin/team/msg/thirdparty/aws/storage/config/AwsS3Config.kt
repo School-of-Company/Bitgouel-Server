49c3152b5f7c04d8c188f5681b96ac0e78a69e2a
JuuuuHong <juhang720@gmail.com>
2024-05-07 11:04:39 +0900

    add :: aws s3 configuration

--- bitgouel-api/src/main/kotlin/team/msg/thirdparty/aws/storage/config/AwsS3Config.kt
+++ bitgouel-api/src/main/kotlin/team/msg/thirdparty/aws/storage/config/AwsS3Config.kt
@@ -1,5 +1,10 @@
 package team.msg.thirdparty.aws.storage.config
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import team.msg.thirdparty.aws.AwsProperties
@@ -8,4 +13,12 @@ import team.msg.thirdparty.aws.AwsProperties
 class AwsS3Config(
     private val awsProperties: AwsProperties
 ) {
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        val awsCredentials = BasicAWSCredentials(awsProperties.accessKey, awsProperties.secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build()
+    }
 }
