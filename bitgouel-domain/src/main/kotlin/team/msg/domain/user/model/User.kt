2f86ffcf2aa9e3e66ff7f6a7217f6e56c2efaa23
JuuuuHong <juhang720@gmail.com>
2024-05-30 17:04:52 +0900

    update :: email, password columnDefinition

--- bitgouel-domain/src/main/kotlin/team/msg/domain/user/model/User.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/user/model/User.kt
@@ -15,7 +15,7 @@ class User(
     @get:JvmName("getIdentifier")
     override var id: UUID,
 
-    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+    @Column(columnDefinition = "VARCHAR(100) COLLATE utf8mb4_unicode_ci", nullable = false)
     val email: String,
 
     @Column(columnDefinition = "VARCHAR(10)", nullable = false)
@@ -24,7 +24,7 @@ class User(
     @Column(columnDefinition = "VARCHAR(20)", nullable = false)
     val phoneNumber: String,
 
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    @Column(columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci", nullable = false)
     val password: String,
 
     @Enumerated(EnumType.STRING)
