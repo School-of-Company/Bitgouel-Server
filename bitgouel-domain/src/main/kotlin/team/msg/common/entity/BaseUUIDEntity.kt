ed677e24d7d023c3d8467f88b83d1d7afd2a1904
KimTaeO <taeo0328@gmail.com>
2024-06-09 23:11:11 +0900

    delete :: 엔티티 영속화 시 uuid 생성 기능 삭제

--- bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseUUIDEntity.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/common/entity/BaseUUIDEntity.kt
@@ -14,8 +14,6 @@ import javax.persistence.MappedSuperclass
 abstract class BaseUUIDEntity(
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)", nullable = false)
     @get:JvmName(name = "getIdentifier")
     open var id: UUID = UUID(0, 0)
