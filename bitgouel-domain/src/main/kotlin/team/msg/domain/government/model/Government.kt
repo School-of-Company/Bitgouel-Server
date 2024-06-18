2bdf546fcdc0a247d086bfc492ae457e22771dce
JuuuuHong <juhang720@gmail.com>
2024-06-18 14:38:01 +0900

    add :: government entity

---
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/government/model/Government.kt
@@ -0,0 +1,24 @@
+package team.msg.domain.government.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import team.msg.common.enums.Industry
+
+@Entity
+class Government(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = false)
+    val id: Long = 0,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "industry", columnDefinition = "VARCHAR(100)", nullable = false)
+    val industry: Industry
+
+)
