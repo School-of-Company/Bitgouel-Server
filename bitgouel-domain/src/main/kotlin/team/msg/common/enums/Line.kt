a95d541e90af91dc73193ae389dcf564680fb855
박주홍 <103554978+JuuuuHong@users.noreply.github.com>
2024-06-25 16:19:25 +0900

    Update bitgouel-domain/src/main/kotlin/team/msg/common/enums/Line.kt
    Co-authored-by: 노현주 <106813267+ani2689@users.noreply.github.com>

--- bitgouel-domain/src/main/kotlin/team/msg/common/enums/Line.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/common/enums/Line.kt
@@ -1,10 +1,10 @@
 package team.msg.common.enums
 
 /**
- * 공업계열
- * 상업계열
- * 농업생명/보건의료계열
- * 산업수요 맞춤형계열
+ * INDUSTRY - 공업계열
+ * COMMERCE - 상업계열
+ * AGRICULTURAL_LIFE_HEALTH_CARE - 농업생명/보건의료계열
+ * CUSTOMIZED_INDUSTRIAL_DEMAND - 산업수요 맞춤형계열
  */
 enum class Line {
     INDUSTRY,
