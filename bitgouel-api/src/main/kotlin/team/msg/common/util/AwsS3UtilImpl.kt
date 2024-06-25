29ad973a61377b13f4ddcde0ae7bfd66362e4d5f
JuuuuHong <juhang720@gmail.com>
2024-06-25 16:12:47 +0900

    delete :: print error

--- bitgouel-api/src/main/kotlin/team/msg/common/util/AwsS3UtilImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/common/util/AwsS3UtilImpl.kt
@@ -30,10 +30,8 @@ class AwsS3UtilImpl(
                         CannedAccessControlList.PublicRead
                     )
             )
-        }.onFailure { e ->
-            println(e.message)
-            println(e.printStackTrace())
-            throw InternalServerException("이미지 업로드에 실패했습니다. ${e.message} + ${e.printStackTrace()}")
+        }.onFailure {
+            throw InternalServerException("이미지 업로드에 실패했습니다.")
         }
 
         val imageUrl = amazonS3.getUrl(awsS3Properties.bucket, fileName).toString()
