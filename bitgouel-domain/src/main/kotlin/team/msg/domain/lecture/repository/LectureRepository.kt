693de6880d91b85fb43b55d28f47737901cb400c
KimTaeO <taeo0328@gmail.com>
2024-06-27 16:50:46 +0900

    add :: lecture find by id isdeleted clasue

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/LectureRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/LectureRepository.kt
@@ -1,9 +1,13 @@
 package team.msg.domain.lecture.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import team.msg.domain.lecture.model.Lecture
 import team.msg.domain.lecture.repository.custom.CustomLectureRepository
 import java.util.*
 
 
-interface LectureRepository : JpaRepository<Lecture, UUID>, CustomLectureRepository
+interface LectureRepository : JpaRepository<Lecture, UUID>, CustomLectureRepository {
+    @Query("select l from Lecture l where l.id = :id and l.isDeleted = false ")
+    override fun findById(id: UUID): Optional<Lecture>
+}
