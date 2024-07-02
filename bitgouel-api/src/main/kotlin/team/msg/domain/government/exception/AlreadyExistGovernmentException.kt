7be996f7418fabd4602dd9e1f0d562f1587174ee
노현주 <dev.hyeonju@gmail.com>
2024-07-02 11:06:51 +0900

    create :: 이미 존재하는 유관기관 예외

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/government/exception/AlreadyExistGovernmentException.kt
@@ -0,0 +1,8 @@
+package team.msg.domain.government.exception
+
+import team.msg.domain.government.exception.constant.GovernmentErrorCode
+import team.msg.global.error.exception.BitgouelException
+
+class AlreadyExistGovernmentException (
+    message: String
+) : BitgouelException(message, GovernmentErrorCode.ALREADY_EXIST_GOVERNMENT.status)
