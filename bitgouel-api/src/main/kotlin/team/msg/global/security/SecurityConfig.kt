006888d1ef955529e7b3df6ef4ef11e524738c90
KimTaeO <taeo0328@gmail.com>
2024-07-03 13:43:06 +0900

    add :: create university security configuration

--- bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/security/SecurityConfig.kt
@@ -154,6 +154,9 @@ class SecurityConfig(
             .mvcMatchers(HttpMethod.POST, "/government").hasRole(ADMIN)
             .mvcMatchers(HttpMethod.DELETE, "/government/{id}").hasRole(ADMIN)
 
+            // university
+            .mvcMatchers(HttpMethod.POST, "/university").hasRole(ADMIN)
+
             // actuator
             .mvcMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
 
