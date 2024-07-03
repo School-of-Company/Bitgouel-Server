7ff9588e710402bb3132a0c112c6f93215937d34
노현주 <dev.hyeonju@gmail.com>
2024-07-03 15:38:26 +0900

    add :: @EnableFeignClients 추가

--- bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
+++ bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
@@ -2,7 +2,9 @@ package team.msg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class BitgouelApplication
 
