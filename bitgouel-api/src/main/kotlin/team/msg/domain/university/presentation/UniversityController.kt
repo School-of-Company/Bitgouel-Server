43fc9cbab2455c02b0af19c3de99101d9199e4f0
KimTaeO <taeo0328@gmail.com>
2024-07-03 15:23:12 +0900

    create :: delete university controller

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/UniversityController.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/UniversityController.kt
@@ -2,7 +2,9 @@ package team.msg.domain.university.presentation
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,6 +26,12 @@ class UniversityController(
         val request = universityMapper.createUniversityWebRequestToDto(webRequest)
         universityService.createUniversity(request)
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUniversity(@PathVariable id: Long): ResponseEntity<Unit> {
+        universityService.deleteUniversity(id)
+        return ResponseEntity.noContent().build()
     }
 
     @GetMapping
