e586bd8452e640b38a4f3f68016c5954e8bdbddc
KimTaeO <taeo0328@gmail.com>
2024-07-03 11:20:40 +0900

    delete :: id field

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/request/CreateUniversityRequest.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/request/CreateUniversityRequest.kt
@@ -1,7 +1,6 @@
 package team.msg.domain.university.presentation.data.request
 
 data class CreateUniversityRequest(
-    val id: Long,
     val department: String,
     val universityName: String
 )
