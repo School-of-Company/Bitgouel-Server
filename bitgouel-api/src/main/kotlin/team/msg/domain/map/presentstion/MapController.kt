76360022dbc5be9cf4458206d3b8526da2bb97c6
노현주 <dev.hyeonju@gmail.com>
2024-07-03 17:04:38 +0900

    create :: map controller

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/map/presentstion/MapController.kt
@@ -0,0 +1,19 @@
+package team.msg.domain.map.presentstion
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import team.msg.domain.map.presentstion.res.GetCoordinateResponseData
+import team.msg.domain.map.service.MapService
+
+@RestController
+@RequestMapping("/map")
+class MapController(
+    private val mapService: MapService
+) {
+    fun getCoordinate(@RequestParam address: String): ResponseEntity<GetCoordinateResponseData> {
+        val response = mapService.getCoordinate(address)
+        return ResponseEntity.ok(response)
+    }
+}
