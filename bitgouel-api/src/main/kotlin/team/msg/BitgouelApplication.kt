f75fdbbe63c7d7aec91fa792087846e9eba9f62a
노현주 <dev.hyeonju@gmail.com>
2024-07-03 17:55:19 +0900

    delete :: 중복 어노테이션

--- bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
+++ bitgouel-api/src/main/kotlin/team/msg/BitgouelApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
-@EnableFeignClients
 @SpringBootApplication
 class BitgouelApplication
 
