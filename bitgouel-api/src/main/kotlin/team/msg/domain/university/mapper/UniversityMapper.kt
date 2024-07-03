76442505b660484a2994c2556251ecd4d1395091
김태오 <103710151+KimTaeO@users.noreply.github.com>
2024-07-03 15:52:03 +0900

    Update bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityMapper.kt
    Co-authored-by: 박주홍 <103554978+JuuuuHong@users.noreply.github.com>

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityMapper.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityMapper.kt
@@ -3,6 +3,6 @@ package team.msg.domain.university.mapper
 import team.msg.domain.university.presentation.data.request.CreateUniversityRequest
 import team.msg.domain.university.presentation.data.web.CreateUniversityWebRequest
 
-interface UniversityMapper {
+interface UniversityRequestMapper {
     fun createUniversityWebRequestToDto(webRequest: CreateUniversityWebRequest): CreateUniversityRequest
 }
