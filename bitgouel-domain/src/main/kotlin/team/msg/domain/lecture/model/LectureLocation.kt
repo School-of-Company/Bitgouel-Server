be8a5b25bc0d975ab3a35605e482807a35fecd00
노현주 <dev.hyeonju@gmail.com>
2024-07-04 15:43:10 +0900

    create :: 강의 위치 엔티티

---
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/model/LectureLocation.kt
@@ -0,0 +1,31 @@
+package team.msg.domain.lecture.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import java.util.UUID
+
+@Entity
+class LectureLocation(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = 0,
+
+    @Column(name = "user_id", columnDefinition = "BINARY(16)")
+    val lectureId: UUID,
+
+    @Column(name = "x", columnDefinition = "VARCHAR(30)", nullable = false)
+    val x: String,
+
+    @Column(name = "y", columnDefinition = "VARCHAR(30)", nullable = false)
+    val y: String,
+
+    @Column(name = "address", nullable = false)
+    val address: String,
+
+    @Column(name = "details", nullable = false)
+    val details: String
+)
