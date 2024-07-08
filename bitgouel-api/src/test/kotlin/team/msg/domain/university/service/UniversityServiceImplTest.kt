a9b2326030e3c6e39f9b649907946fa0d9c43f6a
KimTaeO <taeo0328@gmail.com>
2024-07-08 19:16:30 +0900

    add :: delete university test code

--- bitgouel-api/src/test/kotlin/team/msg/domain/university/service/UniversityServiceImplTest.kt
+++ bitgouel-api/src/test/kotlin/team/msg/domain/university/service/UniversityServiceImplTest.kt
@@ -4,10 +4,14 @@ import com.appmattus.kotlinfixture.kotlinFixture
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import org.springframework.data.repository.findByIdOrNull
 import team.msg.domain.university.exception.AlreadyExistUniversityException
+import team.msg.domain.university.exception.UniversityHasProfessorConstraintException
 import team.msg.domain.university.model.University
 import team.msg.domain.university.presentation.data.request.CreateUniversityRequest
 import team.msg.domain.university.repository.ProfessorRepository
@@ -55,6 +59,35 @@ class UniversityServiceImplTest : BehaviorSpec({
             Then("AlreadyExistUniversityException 이 발생해야 한다") {
                 shouldThrow<AlreadyExistUniversityException> {
                     universityServiceImpl.createUniversity(request)
+                }
+            }
+        }
+    }
+
+    // deleteUniversity 테스트 코드
+    Given("UniversityId 가 주어졌을 때") {
+        val universityId = 1L
+
+        val university = fixture<University>()
+
+        every { universityRepository.findByIdOrNull(universityId) } returns university
+        every { professorRepository.existsByUniversity(university) } returns false
+        every { universityRepository.delete(university) } just Runs
+
+        When("대학 삭제 시") {
+            universityServiceImpl.deleteUniversity(universityId)
+
+            Then("University 가 삭제가 되어야 한다") {
+                verify(exactly = 1) { universityRepository.delete(university) }
+            }
+        }
+
+        When("University 와 연관 관계를 가진 Professor 가 존재한다면") {
+            every { professorRepository.existsByUniversity(university) } returns true
+
+            Then("UniversityHasProfessorConstraintException 이 발생해야 한다") {
+                shouldThrow<UniversityHasProfessorConstraintException> {
+                    universityServiceImpl.deleteUniversity(universityId)
                 }
             }
         }
