c29df4629004655b7fb0e3cf9f2ed39e41d0c570
KimTaeO <taeo0328@gmail.com>
2024-07-09 15:52:27 +0900

    add :: create department request mapper

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestMapper.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestMapper.kt
@@ -1,8 +1,11 @@
 package team.msg.domain.university.mapper
 
+import team.msg.domain.university.presentation.data.request.CreateDepartmentRequest
 import team.msg.domain.university.presentation.data.request.CreateUniversityRequest
+import team.msg.domain.university.presentation.data.web.CreateDepartmentWebRequest
 import team.msg.domain.university.presentation.data.web.CreateUniversityWebRequest
 
 interface UniversityRequestMapper {
     fun createUniversityWebRequestToDto(webRequest: CreateUniversityWebRequest): CreateUniversityRequest
+    fun createDepartmentWebRequestToDto(webRequest: CreateDepartmentWebRequest): CreateDepartmentRequest
 }
--- bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestRequestMapperImpl.kt
@@ -1,7 +1,9 @@
 package team.msg.domain.university.mapper
 
 import org.springframework.stereotype.Component
+import team.msg.domain.university.presentation.data.request.CreateDepartmentRequest
 import team.msg.domain.university.presentation.data.request.CreateUniversityRequest
+import team.msg.domain.university.presentation.data.web.CreateDepartmentWebRequest
 import team.msg.domain.university.presentation.data.web.CreateUniversityWebRequest
 
 @Component
@@ -10,5 +12,10 @@ class UniversityRequestRequestMapperImpl : UniversityRequestMapper {
     override fun createUniversityWebRequestToDto(webRequest: CreateUniversityWebRequest) : CreateUniversityRequest =
         CreateUniversityRequest(
             universityName = webRequest.universityName
+        )
+
+    override fun createDepartmentWebRequestToDto(webRequest: CreateDepartmentWebRequest): CreateDepartmentRequest =
+        CreateDepartmentRequest(
+            department = webRequest.department
         )
 }
