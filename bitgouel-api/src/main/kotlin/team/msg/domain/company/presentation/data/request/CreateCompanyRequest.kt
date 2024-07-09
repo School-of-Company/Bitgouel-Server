50d08a54f67d6add7738a06bafc46f111f5a54ad
노현주 <dev.hyeonju@gmail.com>
2024-07-09 09:37:18 +0900

    delete :: create company dto에서 id 삭제

--- bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/data/request/CreateCompanyRequest.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/data/request/CreateCompanyRequest.kt
@@ -3,7 +3,6 @@ package team.msg.domain.company.presentation.data.request
 import team.msg.common.enums.Field
 
 data class CreateCompanyRequest(
-    val id: Long,
     val companyName: String,
     val field: Field
 )
--- bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/web/CreateCompanyWebRequest.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/web/CreateCompanyWebRequest.kt
@@ -2,14 +2,12 @@ package team.msg.domain.company.presentation.web
 
 import javax.validation.constraints.NotBlank
 import org.jetbrains.annotations.NotNull
+import team.msg.common.enums.Field
 
 data class CreateCompanyWebRequest(
-    @field:NotNull
-    val id: Long,
-
     @field:NotBlank
     val companyName: String,
 
-    @field:NotBlank
-    val field: String
+    @field:NotNull
+    val field: Field
 )
