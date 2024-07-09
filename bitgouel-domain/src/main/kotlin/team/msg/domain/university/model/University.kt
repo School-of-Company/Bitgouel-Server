66b8c80cf6c08434c79cbbaf5505125953148888
KimTaeO <taeo0328@gmail.com>
2024-07-09 15:01:00 +0900

    update :: university entity

--- bitgouel-domain/src/main/kotlin/team/msg/domain/university/model/University.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/university/model/University.kt
@@ -1,10 +1,13 @@
 package team.msg.domain.university.model
 
+import javax.persistence.CollectionTable
 import javax.persistence.Column
+import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
 
 @Entity
 class University(
@@ -14,10 +17,10 @@ class University(
     @Column(name = "id", nullable = false)
     val id: Long = 0,
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", unique = true, nullable = false)
     val name: String,
 
-    @Column(name = "department", columnDefinition = "VARCHAR(50)", nullable = false)
-    val department: String
-
+    @ElementCollection
+    @CollectionTable(name = "department", joinColumns = [JoinColumn(name = "university_id")])
+    val department: List<String>
 )
