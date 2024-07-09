12ef472cf47e362447ffc4563a61d142c90c16fc
노현주 <dev.hyeonju@gmail.com>
2024-07-09 09:17:05 +0900

    update :: field type string -> Field enum

--- bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/data/response/CompanyResponse.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/company/presentation/data/response/CompanyResponse.kt
@@ -1,7 +1,28 @@
 package team.msg.domain.company.presentation.data.response
 
+import team.msg.common.enums.Field
+import team.msg.domain.company.model.Company
+
 data class CompanyResponse(
     val id: Long,
     val companyName: String,
-    val field: String
+    val field: Field
+) {
+    companion object {
+        fun of(company: Company) = CompanyResponse(
+            id = company.id,
+            companyName = company.name,
+            field = company.field
+        )
+
+        fun listOf(companies: List<Company>) = CompaniesResponse(
+            companies.map {
+                of(it)
+            }
+        )
+    }
+}
+
+data class CompaniesResponse(
+    val companies: List<CompanyResponse>
 )
