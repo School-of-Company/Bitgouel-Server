2ee05d1d7d962b1cb934d6c60b6873844032ad4a
JuuuuHong <juhang720@gmail.com>
2024-07-09 15:50:33 +0900

    update :: create school에서 동아리 생성 분리

--- bitgouel-api/src/main/kotlin/team/msg/domain/school/presentation/data/request/CreateSchoolRequest.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/school/presentation/data/request/CreateSchoolRequest.kt
@@ -1,11 +1,9 @@
 package team.msg.domain.school.presentation.data.request
 
 import team.msg.common.enums.Line
-import team.msg.domain.club.presentation.data.reqeust.CreateClubRequest
 
 data class CreateSchoolRequest(
     val schoolName: String,
     val line: Line,
-    val departments: List<String>,
-    val club: List<CreateClubRequest>
+    val departments: List<String>
 )
