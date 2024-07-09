b15a2a6bccaeff78f4031b945604d8524dd0ba45
KimTaeO <taeo0328@gmail.com>
2024-07-09 19:50:25 +0900

    create :: update university requests

---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/request/UpdateUniversityRequest.kt
@@ -0,0 +1,5 @@
+package team.msg.domain.university.presentation.data.request
+
+data class UpdateUniversityRequest(
+    val universityName: String
+)
---
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/web/UpdateUniversityWebRequest.kt
@@ -0,0 +1,5 @@
+package team.msg.domain.university.presentation.data.web
+
+data class UpdateUniversityWebRequest(
+    val universityName: String
+)
