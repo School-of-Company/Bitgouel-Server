1b062bd14f3398e34724b7d5e9ce85d52fcc8bf8
KimTaeO <taeo0328@gmail.com>
2024-07-09 15:30:01 +0900

    update :: query all university response

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/response/UniversityResponse.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/presentation/data/response/UniversityResponse.kt
@@ -4,17 +4,18 @@ import team.msg.domain.university.model.University
 
 data class UniversityResponse(
     val id: Long,
-    val department: String,
-    val universityName: String
+    val universityName: String,
+    val departments: List<String>
 ) {
     companion object {
 
         fun of(university: University) =
             UniversityResponse(
                 id = university.id,
-                department = university.department,
+                departments = university.departments,
                 universityName = university.name
             )
+
 
         fun listOf(universities: List<University>) =
             universities.map { of(it) }
