2da18501feba9ff669f050edc9164ad2d87c2db7
KimTaeO <taeo0328@gmail.com>
2024-07-09 19:57:42 +0900

    add :: update university request mapper

--- bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestRequestMapperImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/university/mapper/UniversityRequestRequestMapperImpl.kt
@@ -3,14 +3,21 @@ package team.msg.domain.university.mapper
 import org.springframework.stereotype.Component
 import team.msg.domain.university.presentation.data.request.CreateDepartmentRequest
 import team.msg.domain.university.presentation.data.request.CreateUniversityRequest
+import team.msg.domain.university.presentation.data.request.UpdateUniversityRequest
 import team.msg.domain.university.presentation.data.web.CreateDepartmentWebRequest
 import team.msg.domain.university.presentation.data.web.CreateUniversityWebRequest
+import team.msg.domain.university.presentation.data.web.UpdateUniversityWebRequest
 
 @Component
 class UniversityRequestRequestMapperImpl : UniversityRequestMapper {
 
     override fun createUniversityWebRequestToDto(webRequest: CreateUniversityWebRequest) : CreateUniversityRequest =
         CreateUniversityRequest(
+            universityName = webRequest.universityName
+        )
+
+    override fun updateUniversityWebRequestToDto(webRequest: UpdateUniversityWebRequest): UpdateUniversityRequest =
+        UpdateUniversityRequest(
             universityName = webRequest.universityName
         )
 
