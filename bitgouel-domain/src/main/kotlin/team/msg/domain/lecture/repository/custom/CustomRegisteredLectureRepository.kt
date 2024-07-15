02e5dc0fdcd05debe927301c22c1ef618acf93ae
노현주 <dev.hyeonju@gmail.com>
2024-07-15 14:02:18 +0900

    update :: 프로젝션 이름 변경

--- bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/CustomRegisteredLectureRepository.kt
+++ bitgouel-domain/src/main/kotlin/team/msg/domain/lecture/repository/custom/CustomRegisteredLectureRepository.kt
@@ -1,14 +1,14 @@
 package team.msg.domain.lecture.repository.custom
 
 import team.msg.domain.lecture.model.RegisteredLecture
-import team.msg.domain.lecture.repository.custom.projection.LectureAndIsCompleteProjection
+import team.msg.domain.lecture.repository.custom.projection.LectureAndRegisteredProjection
 import team.msg.domain.lecture.repository.custom.projection.SignedUpStudentProjection
 import java.util.*
 
 interface CustomRegisteredLectureRepository {
     fun deleteAllByStudentId(studentId: UUID)
     fun existsOne(studentId: UUID,lectureId: UUID): Boolean
-    fun findLecturesAndIsCompleteByStudentId(studentId: UUID): List<LectureAndIsCompleteProjection>
+    fun findLecturesAndIsCompleteByStudentId(studentId: UUID): List<LectureAndRegisteredProjection>
     fun findSignedUpStudentsByLectureId(lectureId: UUID): List<SignedUpStudentProjection>
     fun findSignedUpStudentsByLectureIdAndClubId(lectureId: UUID, clubId: Long): List<SignedUpStudentProjection>
     fun findByLectureIdAndStudentId(lectureId: UUID, studentId: UUID): RegisteredLecture?
