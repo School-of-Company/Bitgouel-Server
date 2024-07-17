b44f6ff188e980cf3f98591310a86f6d38ccc53c
JuuuuHong <juhang720@gmail.com>
2024-07-17 20:52:31 +0900

    add :: company warm up

--- bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
@@ -4,17 +4,20 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 import team.msg.thirdparty.feign.club.client.ClubClient
+import team.msg.thirdparty.feign.company.client.CompanyClient
 import team.msg.thirdparty.feign.faq.client.FaqClient
 
 @Component
 class WarmUpRunner(
     private val faqClient: FaqClient,
-    private val clubClient: ClubClient
+    private val clubClient: ClubClient,
+    private val companyClient: CompanyClient
 ): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         runCatching {
             faqClient.queryFaq()
             clubClient.queryAllClubNames(null)
+            companyClient.queryCompanies()
         }.onFailure {  }
     }
 }
---
+++ bitgouel-api/src/main/kotlin/team/msg/thirdparty/feign/company/client/CompanyClient.kt
@@ -0,0 +1,13 @@
+package team.msg.thirdparty.feign.company.client
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import team.msg.domain.company.presentation.data.response.CompaniesResponse
+
+@FeignClient(value = "company", url = "http://localhost:8080")
+interface CompanyClient {
+
+    @GetMapping("/company")
+    fun queryCompanies(): CompaniesResponse
+
+}
