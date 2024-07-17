d56a12d052e3b0ab7867807d6205407f645e928b
JuuuuHong <juhang720@gmail.com>
2024-07-17 20:53:49 +0900

    add :: government warm up

--- bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
@@ -6,18 +6,21 @@ import org.springframework.stereotype.Component
 import team.msg.thirdparty.feign.club.client.ClubClient
 import team.msg.thirdparty.feign.company.client.CompanyClient
 import team.msg.thirdparty.feign.faq.client.FaqClient
+import team.msg.thirdparty.feign.government.client.GovernmentClient
 
 @Component
 class WarmUpRunner(
     private val faqClient: FaqClient,
     private val clubClient: ClubClient,
-    private val companyClient: CompanyClient
+    private val companyClient: CompanyClient,
+    private val governmentClient: GovernmentClient
 ): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         runCatching {
             faqClient.queryFaq()
             clubClient.queryAllClubNames(null)
             companyClient.queryCompanies()
+            governmentClient.queryGovernments()
         }.onFailure {  }
     }
 }
