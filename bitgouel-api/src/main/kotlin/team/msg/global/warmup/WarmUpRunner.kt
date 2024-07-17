e8e023b2eef6e2b9436b3c2ba59c776df854f5f2
JuuuuHong <juhang720@gmail.com>
2024-07-17 20:50:51 +0900

    add :: club warm up

--- bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/warmup/WarmUpRunner.kt
@@ -3,15 +3,18 @@ package team.msg.global.warmup
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
+import team.msg.thirdparty.feign.club.client.ClubClient
 import team.msg.thirdparty.feign.faq.client.FaqClient
 
 @Component
 class WarmUpRunner(
-    private val faqClient: FaqClient
+    private val faqClient: FaqClient,
+    private val clubClient: ClubClient
 ): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         runCatching {
             faqClient.queryFaq()
+            clubClient.queryAllClubNames(null)
         }.onFailure {  }
     }
 }
