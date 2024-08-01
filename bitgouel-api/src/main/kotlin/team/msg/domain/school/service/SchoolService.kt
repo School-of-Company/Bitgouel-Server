b6b392cf9ff33a2b6c84286c285f897cb9e0ec81
노현주 <dev.hyeonju@gmail.com>
2024-08-01 16:44:39 +0900

    add :: query school details service

--- bitgouel-api/src/main/kotlin/team/msg/domain/school/service/SchoolService.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/school/service/SchoolService.kt
@@ -4,10 +4,12 @@ import org.springframework.web.multipart.MultipartFile
 import team.msg.domain.school.presentation.data.request.CreateSchoolRequest
 import team.msg.domain.school.presentation.data.request.UpdateSchoolRequest
 import team.msg.domain.school.presentation.data.response.SchoolNamesResponse
+import team.msg.domain.school.presentation.data.response.SchoolResponse
 import team.msg.domain.school.presentation.data.response.SchoolsResponse
 
 interface SchoolService {
     fun querySchools(): SchoolsResponse
+    fun querySchoolDetails(id: Long): SchoolResponse
     fun querySchoolNames(): SchoolNamesResponse
     fun createSchool(request: CreateSchoolRequest, logoImage: MultipartFile)
     fun updateSchool(id: Long, request: UpdateSchoolRequest, logoImage: MultipartFile)
