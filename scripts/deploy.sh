9e9b76d0e58b4c9ff28ee8c10190ca0b4c2f22e9
JuuuuHong <juhang720@gmail.com>
2024-08-12 10:04:57 +0900

    add :: auto commit false

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,16 @@
-DEPLOYMENT_ACTIVE_PROFILE=$(echo ACTIVE_PROFILES)
+DEPLOYMENT_ACTIVE_PROFILES=$(echo ACTIVE_PROFILES)
 SCRIPT_PATH="/home/ec2-user/action/scripts"
 
-if [ -z "$DEPLOYMENT_ACTIVE_PROFILE" ]; then
+if [ -z "$DEPLOYMENT_ACTIVE_PROFILES" ]; then
   echo "Active profiles 가 설정되어 있지 않습니다."
   exit 1
 fi
 
-if [ "$DEPLOYMENT_ACTIVE_PROFILE" == "dev" ]; then
+if [ "$DEPLOYMENT_ACTIVE_PROFILES" == "dev" ]; then
   chmod +x $SCRIPT_PATH/dev-deploy.sh
   $SCRIPT_PATH/dev-deploy.sh
 
-elif [ "$DEPLOYMENT_ACTIVE_PROFILE" == "prod" ]; then
+elif [ "$DEPLOYMENT_ACTIVE_PROFILES" == "prod" ]; then
   chmod +x $SCRIPT_PATH/prod-deploy.sh
   $SCRIPT_PATH/prod-deploy.sh
 
