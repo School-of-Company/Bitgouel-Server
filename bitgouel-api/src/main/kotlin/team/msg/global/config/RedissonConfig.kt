ac5a2086c662d05c37614c1af52636057b4ca5f5
KimTaeO <taeo0328@gmail.com>
2024-08-20 14:25:56 +0900

    add :: max min redisson connection size

--- bitgouel-api/src/main/kotlin/team/msg/global/config/RedissonConfig.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/config/RedissonConfig.kt
@@ -21,9 +21,12 @@ class RedissonConfig(
         val config = Config()
         val codec = StringCodec()
         config.codec = codec
-        config.useSingleServer().setAddress(
-            "$REDIS_PREFIX${redisProperties.host}:${redisProperties.port}"
-        )
+        config.useSingleServer()
+            .setAddress(
+                "$REDIS_PREFIX${redisProperties.host}:${redisProperties.port}"
+            )
+            .setConnectionPoolSize(10)
+            .setConnectionMinimumIdleSize(5)
         return Redisson.create(config)
     }
 }
