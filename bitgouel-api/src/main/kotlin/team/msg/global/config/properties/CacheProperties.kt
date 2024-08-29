3597b1cd9d1cb47c1b050dea42dc2ab6fea74181
KimTaeO <taeo0328@gmail.com>
2024-08-29 20:45:34 +0900

    update :: cache expire timeout

--- bitgouel-api/src/main/kotlin/team/msg/global/config/properties/CacheProperties.kt
+++ bitgouel-api/src/main/kotlin/team/msg/global/config/properties/CacheProperties.kt
@@ -6,8 +6,8 @@ enum class CacheProperties(
     val expiredAfterWrite: Long
 ) {
     CLUB_PROFILE("queryClubs", 50L, 30L),
-    UNIVERSITY_PROFILE("queryUniversities", 2L, 30L),
-    SCHOOL_PROFILE("querySchools", 2L, 30L),
-    COMPANY_PROFILE("queryCompanies", 2L, 30L),
-    GOVERNMENT_PROFILE("queryGovernments", 2L, 30L)
+    UNIVERSITY_PROFILE("queryUniversities", 2L, 60L),
+    SCHOOL_PROFILE("querySchools", 2L, 60L),
+    COMPANY_PROFILE("queryCompanies", 2L, 60L),
+    GOVERNMENT_PROFILE("queryGovernments", 2L, 60L)
 }
