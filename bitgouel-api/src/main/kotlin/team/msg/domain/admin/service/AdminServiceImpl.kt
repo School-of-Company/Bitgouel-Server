4eaba1d79f8c194b338de8f7c0383f44d93d6000
노현주 <dev.hyeonju@gmail.com>
2024-09-02 01:30:05 +0900

    update :: workbook 밖으로 빼기

--- bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
+++ bitgouel-api/src/main/kotlin/team/msg/domain/admin/service/AdminServiceImpl.kt
@@ -139,41 +139,43 @@ class AdminServiceImpl(
      */
     @Transactional(rollbackFor = [Exception::class])
     override fun uploadStudentListExcel(file: MultipartFile) {
-        file.inputStream.use {
-            val sheet = try {
+        val workbook = file.inputStream.use {
+            try {
                 WorkbookFactory.create(it)
             } catch (e: IndexOutOfBoundsException) {
                 throw InvalidCellTypeException("전화번호 셀 서식을 텍스트로 바꿔주세요.")
             } catch (e: Exception) {
                 throw InternalServerException("엑셀 파일 처리 중 문제가 발생했습니다. info : [ errorMessage = ${e.message} ]")
-            }.getSheetAt(0)
-
-            sheet.forEachIndexed { index, row ->
-                if (index == 0)
-                    return@forEachIndexed
-
-                if (row.getCell(0).stringCellValue == "")
-                    return
-
-                val email = row.getCell(0).stringCellValue
-                val name = row.getCell(1).stringCellValue
-                val phoneNumber = row.getCell(2).stringCellValue
-                val password = row.getCell(3).stringCellValue
-                val clubName = row.getCell(4).stringCellValue
-                val grade = row.getCell(5).numericCellValue.toInt()
-                val classRoom = row.getCell(6).numericCellValue.toInt()
-                val number = row.getCell(7).numericCellValue.toInt()
-                val admissionNumber = row.getCell(8).numericCellValue.toInt()
-                val subscriptionGrade = row.getCell(9).numericCellValue.toInt()
-
-                validateExcelStudentData(email, phoneNumber, password)
-
-                val user = userUtil.createUser(email, name, phoneNumber, password, Authority.ROLE_STUDENT)
-
-                val club = clubRepository findByName clubName
-
-                studentUtil.createStudent(user, club, grade, classRoom, number, admissionNumber, subscriptionGrade)
             }
+        }
+
+        val sheet = workbook.getSheetAt(0)
+
+        sheet.forEachIndexed { index, row ->
+            if (index == 0)
+                return@forEachIndexed
+
+            if (row.getCell(0).stringCellValue == "")
+                return
+
+            val email = row.getCell(0).stringCellValue
+            val name = row.getCell(1).stringCellValue
+            val phoneNumber = row.getCell(2).stringCellValue
+            val password = row.getCell(3).stringCellValue
+            val clubName = row.getCell(4).stringCellValue
+            val grade = row.getCell(5).numericCellValue.toInt()
+            val classRoom = row.getCell(6).numericCellValue.toInt()
+            val number = row.getCell(7).numericCellValue.toInt()
+            val admissionNumber = row.getCell(8).numericCellValue.toInt()
+            val subscriptionGrade = row.getCell(9).numericCellValue.toInt()
+
+            validateExcelStudentData(email, phoneNumber, password)
+
+            val user = userUtil.createUser(email, name, phoneNumber, password, Authority.ROLE_STUDENT)
+
+            val club = clubRepository findByName clubName
+
+            studentUtil.createStudent(user, club, grade, classRoom, number, admissionNumber, subscriptionGrade)
         }
     }
 
@@ -183,47 +185,49 @@ class AdminServiceImpl(
      */
     @Transactional(rollbackFor = [Exception::class])
     override fun uploadClubListExcel(file: MultipartFile) {
-        file.inputStream.use {
-            val sheet = try {
+        val workbook = file.inputStream.use {
+            try {
                 WorkbookFactory.create(it)
             } catch (e: Exception) {
                 throw InternalServerException("엑셀 파일 처리 중 문제가 발생했습니다. info : [ errorMessage = ${e.message} ]")
-            }.getSheetAt(0)
-
-            sheet.forEachIndexed { index, row ->
-                if (index == 0)
-                    return@forEachIndexed
-
-                if (row.getCell(0).stringCellValue == "")
-                    return
-
-                val schoolName = row.getCell(0).stringCellValue
-                val clubName = row.getCell(1).stringCellValue
-                val field = row.getCell(2).stringCellValue
-
-                val school = schoolRepository.findByName(schoolName)
-                    ?: throw SchoolNotFoundException("존재하지 않는 학교입니다. info : [ schoolName = $schoolName ]")
-
-                if (clubRepository.existsByName(clubName)) {
-                    throw AlreadyExistClubException("이미 존재하는 동아리입니다. info : [ clubName = $clubName ]")
-                }
-
-                val clubField = when (field) {
-                    FUTURISTIC_TRANSPORTATION_EQUIPMENT -> Field.FUTURISTIC_TRANSPORTATION_EQUIPMENT
-                    ENERGY -> Field.ENERGY
-                    MEDICAL_HEALTHCARE -> Field.MEDICAL_HEALTHCARE
-                    AI_CONVERGENCE -> Field.AI_CONVERGENCE
-                    CULTURE -> Field.CULTURE
-                    else -> throw InvalidFieldException("유효하지 않은 동아리 분야입니다. info : [ clubField = $field ]")
-                }
-
-                val club = Club(
-                    school = school,
-                    name = clubName,
-                    field = clubField
-                )
-                clubRepository.save(club)
             }
+        }
+
+        val sheet = workbook.getSheetAt(0)
+
+        sheet.forEachIndexed { index, row ->
+            if (index == 0)
+                return@forEachIndexed
+
+            if (row.getCell(0).stringCellValue == "")
+                return
+
+            val schoolName = row.getCell(0).stringCellValue
+            val clubName = row.getCell(1).stringCellValue
+            val field = row.getCell(2).stringCellValue
+
+            val school = schoolRepository.findByName(schoolName)
+                ?: throw SchoolNotFoundException("존재하지 않는 학교입니다. info : [ schoolName = $schoolName ]")
+
+            if (clubRepository.existsByName(clubName)) {
+                throw AlreadyExistClubException("이미 존재하는 동아리입니다. info : [ clubName = $clubName ]")
+            }
+
+            val clubField = when (field) {
+                FUTURISTIC_TRANSPORTATION_EQUIPMENT -> Field.FUTURISTIC_TRANSPORTATION_EQUIPMENT
+                ENERGY -> Field.ENERGY
+                MEDICAL_HEALTHCARE -> Field.MEDICAL_HEALTHCARE
+                AI_CONVERGENCE -> Field.AI_CONVERGENCE
+                CULTURE -> Field.CULTURE
+                else -> throw InvalidFieldException("유효하지 않은 동아리 분야입니다. info : [ clubField = $field ]")
+            }
+
+            val club = Club(
+                school = school,
+                name = clubName,
+                field = clubField
+            )
+            clubRepository.save(club)
         }
     }
 
@@ -233,102 +237,104 @@ class AdminServiceImpl(
      */
     @Transactional(rollbackFor = [Exception::class])
     override fun uploadLectureListExcel(file: MultipartFile) {
-        file.inputStream.use {
-            val sheet = try {
+        val workbook = file.inputStream.use {
+            try {
                 WorkbookFactory.create(it)
             } catch (e: IndexOutOfBoundsException) {
                 throw InvalidCellTypeException("셀 서식을 텍스트로 변경해주세요.")
             } catch (e: Exception) {
                 throw InternalServerException("엑셀 파일 처리 중 문제가 발생했습니다. info : [ errorMessage = ${e.message} ]")
-            }.getSheetAt(0)
+            }
+        }
 
-            sheet.forEachIndexed { index, row ->
-                if (index == 0 || index == 1)
-                    return@forEachIndexed
+        val sheet = workbook.getSheetAt(0)
 
-                if (row.getCell(0) == null || row.getCell(0).stringCellValue == "")
-                    return
+        sheet.forEachIndexed { index, row ->
+            if (index == 0 || index == 1)
+                return@forEachIndexed
 
-                val name = row.getCell(0).stringCellValue
-                val content = row.getCell(1).stringCellValue
+            if (row.getCell(0) == null || row.getCell(0).stringCellValue == "")
+                return
 
-                val instructorName = row.getCell(2).stringCellValue
-                val instructorEmail = row.getCell(3).stringCellValue
+            val name = row.getCell(0).stringCellValue
+            val content = row.getCell(1).stringCellValue
 
-                val instructor = userRepository.findByEmail(instructorEmail)
-                    ?: throw UserNotFoundException("존재하지 않는 강사입니다. info : [ email = $instructorEmail ]")
+            val instructorName = row.getCell(2).stringCellValue
+            val instructorEmail = row.getCell(3).stringCellValue
 
-                val type = row.getCell(4).stringCellValue
-                val credit = row.getCell(5).numericCellValue.toInt()
-                val semester = row.getCell(6).stringCellValue
+            val instructor = userRepository.findByEmail(instructorEmail)
+                ?: throw UserNotFoundException("존재하지 않는 강사입니다. info : [ email = $instructorEmail ]")
 
-                val lectureSemester = when(semester) {
-                    FIRST_YEAR_FALL_SEMESTER -> Semester.FIRST_YEAR_FALL_SEMESTER
-                    SECOND_YEAR_SPRING_SEMESTER -> Semester.SECOND_YEAR_SPRING_SEMESTER
-                    SECOND_YEAR_FALL_SEMESTER -> Semester.SECOND_YEAR_FALL_SEMESTER
-                    THIRD_YEAR_SPRING_SEMESTER -> Semester.THIRD_YEAR_SPRING_SEMESTER
-                    else -> Semester.NOT_APPLICABLE
+            val type = row.getCell(4).stringCellValue
+            val credit = row.getCell(5).numericCellValue.toInt()
+            val semester = row.getCell(6).stringCellValue
+
+            val lectureSemester = when(semester) {
+                FIRST_YEAR_FALL_SEMESTER -> Semester.FIRST_YEAR_FALL_SEMESTER
+                SECOND_YEAR_SPRING_SEMESTER -> Semester.SECOND_YEAR_SPRING_SEMESTER
+                SECOND_YEAR_FALL_SEMESTER -> Semester.SECOND_YEAR_FALL_SEMESTER
+                THIRD_YEAR_SPRING_SEMESTER -> Semester.THIRD_YEAR_SPRING_SEMESTER
+                else -> Semester.NOT_APPLICABLE
+            }
+
+            val division = row.getCell(7).stringCellValue
+            val line = row.getCell(8).stringCellValue
+            val department = row.getCell(9).stringCellValue
+            val maxRegisteredUser = row.getCell(10).numericCellValue.toInt()
+
+            val startDate = row.getCell(11).stringCellValue.toLocalDateTime()
+            val endDate = row.getCell(12).stringCellValue.toLocalDateTime()
+            val essentialComplete = row.getCell(16).stringCellValue
+
+            val lectureEssentialComplete = essentialComplete.uppercase() == "O"
+
+            val lecture = Lecture(
+                id = UUID(0, 0),
+                name = name,
+                content = content,
+                instructor = instructorName,
+                user = instructor,
+                lectureType = type,
+                credit = credit,
+                semester = lectureSemester,
+                division = division,
+                line = line,
+                department = department,
+                maxRegisteredUser = maxRegisteredUser,
+                startDate = startDate,
+                endDate = endDate,
+                essentialComplete = lectureEssentialComplete
+            )
+
+            lectureRepository.save(lecture)
+
+            val lectureDates = row.getCell(13).stringCellValue
+                .split(",").map {
+                    val timeZone = it.split(" ", "~")
+                    LectureDate(
+                        id = UUID(0, 0),
+                        lecture = lecture,
+                        completeDate = timeZone[0].toLocalDate(),
+                        startTime = timeZone[1].toLocalTime(),
+                        endTime = timeZone[2].toLocalTime()
+                    )
                 }
 
-                val division = row.getCell(7).stringCellValue
-                val line = row.getCell(8).stringCellValue
-                val department = row.getCell(9).stringCellValue
-                val maxRegisteredUser = row.getCell(10).numericCellValue.toInt()
+            lectureDateRepository.saveAll(lectureDates)
 
-                val startDate = row.getCell(11).stringCellValue.toLocalDateTime()
-                val endDate = row.getCell(12).stringCellValue.toLocalDateTime()
-                val essentialComplete = row.getCell(16).stringCellValue
+            val address = row.getCell(14).stringCellValue
+            val addressDetails = row.getCell(15).stringCellValue
 
-                val lectureEssentialComplete = essentialComplete.uppercase() == "O"
+            val coordinate = kakaoUtil.getCoordinate(address)
+            val lectureLocation = LectureLocation(
+                lectureId = lecture.id,
+                x = coordinate.first,
+                y = coordinate.second,
+                address = address,
+                details = addressDetails
+            )
 
-                val lecture = Lecture(
-                    id = UUID(0, 0),
-                    name = name,
-                    content = content,
-                    instructor = instructorName,
-                    user = instructor,
-                    lectureType = type,
-                    credit = credit,
-                    semester = lectureSemester,
-                    division = division,
-                    line = line,
-                    department = department,
-                    maxRegisteredUser = maxRegisteredUser,
-                    startDate = startDate,
-                    endDate = endDate,
-                    essentialComplete = lectureEssentialComplete
-                )
-
-                lectureRepository.save(lecture)
-
-                val lectureDates = row.getCell(13).stringCellValue
-                    .split(",").map {
-                        val timeZone = it.split(" ", "~")
-                        LectureDate(
-                            id = UUID(0, 0),
-                            lecture = lecture,
-                            completeDate = timeZone[0].toLocalDate(),
-                            startTime = timeZone[1].toLocalTime(),
-                            endTime = timeZone[2].toLocalTime()
-                        )
-                    }
-
-                lectureDateRepository.saveAll(lectureDates)
-
-                val address = row.getCell(14).stringCellValue
-                val addressDetails = row.getCell(15).stringCellValue
-
-                val coordinate = kakaoUtil.getCoordinate(address)
-                val lectureLocation = LectureLocation(
-                    lectureId = lecture.id,
-                    x = coordinate.first,
-                    y = coordinate.second,
-                    address = address,
-                    details = addressDetails
-                )
-
-                lectureLocationRepository.save(lectureLocation)
-            }
+            lectureLocationRepository.save(lectureLocation)
         }
     }
 
